be2020ee05180676990244b533dc0f51387a80c8
Michael Gorven <michael@gorven.za.net>
2008-12-30 13:35:06 +0200

    bzr: Reformat output.

--- ibid/plugins/bzr.py
+++ ibid/plugins/bzr.py
@@ -1,10 +1,35 @@
 from cStringIO import StringIO
+from time import strftime
+from datetime import datetime
 
 from bzrlib.branch import Branch
 from bzrlib import log
 
 import ibid
 from ibid.plugins import Processor, match
+
+class LogFormatter(log.LogFormatter):
+
+	def ago(self, time):
+		then = datetime.utcfromtimestamp(time)
+		delta = datetime.now() - then
+		ago = ''
+		if delta.days / 365:
+			ago = '%s years' % (delta.days / 365)
+		elif delta.days / 30:
+			ago = '%s months' % (delta.days / 30)
+		elif delta.days:
+			ago = '%s days' % delta.days
+		elif delta.seconds / 3600:
+			ago = '%s hours' % (delta.seconds / 3600)
+		elif delta.seconds / 60:
+			ago = '%s minutes' % (delta.seconds / 60)
+		else:
+			ago = '%s seconds' % delta.seconds
+		return ago
+
+	def log_revision(self, revision):
+		self.to_file.write('Commit %s by %s %s ago: %s\n' % (revision.revno, self.short_author(revision.rev), self.ago(revision.rev.timestamp), revision.rev.message.replace('\n', '')))
 
 class Bazaar(Processor):
 
@@ -23,7 +48,7 @@ class Bazaar(Processor):
 			return
 
 		f=StringIO();
-		log.show_log(self.branch, log.LineLogFormatter(f), start_revision=revno, end_revision=revno, limit=1)
+		log.show_log(self.branch, LogFormatter(f), start_revision=revno, end_revision=revno, limit=1)
 		f.seek(0)
 		commits = f.readlines()
 
