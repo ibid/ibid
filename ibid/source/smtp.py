9c29ef7ea8a1fe475c1f706f503017e57ad8302a
Michael Gorven <michael@gorven.za.net>
2009-01-01 12:31:44 +0200

    Initial version of SMTP server.

--- ibid/source/smtp.py
+++ ibid/source/smtp.py
@@ -1,11 +1,13 @@
 from StringIO import StringIO
 
 from twisted.application import internet
-from twisted.internet import protocol
+from twisted.internet import protocol, defer
 from twisted.mail import smtp
+from zope.interface import implements
 
 import ibid
 from ibid.source import IbidSourceFactory
+from ibid.event import Event
 
 class SMTPClient(smtp.ESMTPClient):
 
@@ -50,10 +52,70 @@ class SMTPClientFactory(protocol.ClientFactory):
 
         return client
 
-class SourceFactory(IbidSourceFactory):
+class IbidDelivery:
+    implements(smtp.IMessageDelivery)
+
+    def __init__(self, name):
+        self.name = name
+
+    def receivedHeader(self, helo, origin, recipients):
+        return 'Received: by Ibid'
+
+    def validateFrom(self, helo, origin):
+        return origin
+
+    def validateTo(self, user):
+        if str(user) == 'ibid@localhost':
+            return lambda: Message(self.name)
+        raise smtp.SMTPBadRcpt(user)
+
+class Message:
+    implements(smtp.IMessage)
+
+    def __init__(self, name):
+        self.lines = []
+        self.name = name
+
+    def lineReceived(self, line):
+        self.lines.append(line)
+
+    def eomReceived(self):
+        headers = {}
+        for line in self.lines:
+            if line == '':
+                break
+            (header, value) = line.split(':')
+            headers[header.strip().lower()] = value.strip()
+
+        event = Event(self.name, 'message')
+        event.sender = headers['from']
+        event.sender_id = event.sender
+        event.who = event.sender
+        event.channel = event.sender
+        event.public = False
+        event.addressed = True
+        event.message = headers['subject']
+
+        ibid.dispatcher.dispatch(event)
+        return defer.succeed(None)
+
+    def connectionLost(self):
+        self.lines = None
+
+class SourceFactory(IbidSourceFactory, smtp.SMTPFactory):
+
+    def __init__(self, name):
+        IbidSourceFactory.__init__(self, name)
+        self.delivery = IbidDelivery(name)
+
+    def buildProtocol(self, addr):
+        p = smtp.SMTPFactory.buildProtocol(self, addr)
+        p.delivery = self.delivery
+        return p
 
     def setServiceParent(self, service):
         self.service = service
+        internet.TCPServer(10025, self).setServiceParent(service)
 
     def send(self, response):
         factory = SMTPClientFactory(self.name, response)
