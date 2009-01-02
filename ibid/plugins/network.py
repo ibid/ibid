ba71d0ba839f5acb6f97c86f80fb483228a5ab9b
Michael Gorven <michael@gorven.za.net>
2009-01-02 22:22:21 +0200

    network.DNS: Modify syntax, allow nameserver to be specified, add SOA.

--- ibid/plugins/network.py
+++ ibid/plugins/network.py
@@ -1,6 +1,6 @@
 import re
 
-from dns.resolver import query, NoAnswer, NXDOMAIN
+from dns.resolver import Resolver, NoAnswer, NXDOMAIN
 from dns.reversename import from_address
 
 from ibid.plugins import Processor, match
@@ -10,22 +10,27 @@ ipaddr = re.compile('\d+\.\d+\.\d+\.\d+')
 
 help['dns'] = u'Performs DNS lookups'
 class DNS(Processor):
-	"""(dns|nslookup|<record type>) [for] <host>"""
+	"""(dns|nslookup|dig) [<record type>] [for] <host> [(from|@) <nameserver>]"""
 
 	feature = 'dns'
 
-	@match(r'^(dns|nslookup|a|aaaa|ptr|ns|cname|mx|txt|spf|srv|sshfp|cert)\s+(?:for\s+)?(.+?)$')
-	def resolve(self, event, record, host):
-		record = record.upper()
-		if record == 'DNS' or record == 'NSLOOKUP':
-			record = 'A'
+	@match(r'^(?:dns|nslookup|dig)(?:\s+(a|aaaa|ptr|ns|soa|cname|mx|txt|spf|srv|sshfp|cert))?\s+(?:for\s+)?(\S+?)(?:\s+(?:from\s+|@)\s*(\S+))?$')
+	def resolve(self, event, record, host, nameserver):
+		if not record:
+			if ipaddr.search(host):
+				host = from_address(host)
+				record = 'PTR'
+			else:
+				record = 'A'
 
-		if ipaddr.search(host):
-			host = from_address(host)
-			record = 'PTR'
+		resolver = Resolver()
+		if nameserver:
+			if not ipaddr.search(nameserver):
+				nameserver = resolver.query(nameserver, 'A')[0].address
+			resolver.nameservers = [nameserver]
 
 		try:
-			answers = query(host, str(record))
+			answers = resolver.query(host, str(record))
 		except NoAnswer:
 			event.addresponse(u"I couldn't find any %s records for %s" % (record, host))
 			return
