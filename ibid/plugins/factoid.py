de53c9dc1135a0bb73b307ae2e3caabbad37d32d
Michael Gorven <michael@gorven.za.net>
2009-01-03 13:28:01 +0200

    factoid: Simple Get processor which handles factoids with arguments

--- ibid/plugins/factoid.py
+++ ibid/plugins/factoid.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from random import choice
+import re
 
 from sqlalchemy import Column, Integer, Unicode, DateTime, Table, MetaData
 from sqlalchemy.orm import relation, mapper
@@ -29,7 +31,6 @@ class Fact(object):
     def __repr__(self):
         return u'<Fact %s %s %s>' % (self.name, self.verb, self.factoid_id)
 
-
 value_table = Table('factoid_values', metadata,
     Column('id', Integer, primary_key=True),
     Column('value', Unicode),
@@ -53,5 +54,26 @@ mapper(Fact, name_table, properties={
     'factoids': relation(Factoid, primaryjoin=value_table.c.factoid_id==name_table.c.factoid_id, foreign_keys=[value_table.c.factoid_id])})
 mapper(Factoid, value_table, properties={
     'facts': relation(Fact, primaryjoin=value_table.c.factoid_id==name_table.c.factoid_id, foreign_keys=[name_table.c.factoid_id])})
+
+sub_percent = re.compile(r'(?<!\\\\)\\%')
+args = ['$one', '$two', '$three', '$four', '$five', '$six', '$seven', '$eight', '$nine', '$ten']
+
+class Get(Processor):
+
+    @match(r'(.*)')
+    def get_factoid(self, event, name):
+
+        session = ibid.databases.ibid()
+        fact = session.query(Fact).filter(":fact LIKE name ESCAPE '\\'").params(fact=name).first()
+        if fact:
+            reply = choice(fact.factoids).value
+            pattern = sub_percent.sub('(.*)', re.escape(fact.name)).replace('\\\\\\%', '%')
+
+            position = 0
+            for capture in re.match(pattern, name).groups():
+                reply = reply.replace(args[position], capture)
+                position = position + 1
+
+            event.addresponse(reply)
 
 # vi: set et sta sw=4 ts=4:
