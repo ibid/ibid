ee8285029e9723ed39d23394b1430a033569d53f
Michael Gorven <michael@gorven.za.net>
2009-01-04 13:17:26 +0200

    basic.SayDo: Add authorisation.

--- ibid/plugins/basic.py
+++ ibid/plugins/basic.py
@@ -1,5 +1,6 @@
 from random import choice
 
+import ibid
 from ibid.plugins import Processor, match, handler
 
 help = {}
@@ -23,17 +24,17 @@ class SayDo(Processor):
 
     @match(r'^(say|do)\s+(\S+)\s+(.*)$')
     def saydo(self, event, action, where, what):
-        if (event.who != u"cocooncrash"):
+        if ibid.auth.authenticate(event) and ibid.auth.authorise(event, 'saydo'):
+            if action.lower() == u"say":
+                reply = {'target': where, 'reply': what}
+            else:
+                reply = {'target': where, 'reply': what, 'action': True}
+        else:
             reply = u"No!  You're not the boss of me!"
             if action.lower() == "say":
                 event['responses'].append({'target': where, 'reply': u"Ooooh! %s was trying to make me say '%s'!" % (event.who, what)})
             else:
                 event['responses'].append({'target': where, 'reply': u"refuses to do '%s' for '%s'" % (what, event.who), 'action': True})
-        else:
-            if action.lower() == u"say":
-                reply = {'target': where, 'reply': what}
-            else:
-                reply = {'target': where, 'reply': what, 'action': True}
 
         event.addresponse(reply)
         return event
