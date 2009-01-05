80bb4640b12b82f46f37e125bc5b34b1169db66d
Adrian Moisey <adrian@cptwks26>
2009-01-05 10:52:03 +0200

    Added RoShamBo module and included it in ibid.ini

---
+++ ibid/plugins/roshambo.py
@@ -0,0 +1,28 @@
+import random
+
+import ibid
+from ibid.plugins import Processor, match
+
+class RoShamBo(Processor):
+	@match(r'^roshambo\s+(rock|paper|scissors)$')
+	def roshambo(self, event, choice):
+		input = choice.lower()
+		cpu = random.randint(0,2)
+		list = ['paper', 'rock', 'scissors']
+		
+		if input == 'paper':
+			input_number = 0
+		elif input == 'rock':
+			input_number = 1
+		elif input == 'scissors':
+			input_number = 2
+	
+		if input_number == cpu:
+			reply = 'Draw %s %s' % list[cpu], input
+		elif (input_number + 1) % 3 == cpu:
+			reply = 'You win! I had: %s, You had: %s' % (list[cpu], input)
+		else:
+			reply = 'I win! I had: %s, You had: %s' % (list[cpu], input)
+	
+		event.addresponse(reply)
+		return event
