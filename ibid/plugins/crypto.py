18cbd7015a414ecda390d599b883a17ff533a7ea
Michael Gorven <michael@gorven.za.net>
2009-01-07 22:25:28 +0200

    Refactor crypto plugin.

--- ibid/plugins/crypto.py
+++ ibid/plugins/crypto.py
@@ -1,5 +1,5 @@
 from crypt import crypt
-from hashlib import md5, sha1, sha224, sha256, sha384, sha512
+import hashlib
 import re
 
 import ibid
@@ -7,26 +7,15 @@ from ibid.plugins import Processor, match, handler
 
 help = {}
 
-hashes =    {   'md5': md5,
-                'sha1': sha1,
-                'sha224': sha224,
-                'sha256': sha256,
-                'sha384': sha384,
-                'sha512': sha512
-            }
-
 help['hash'] = 'Calculates numerous cryptographic hash functions.'
 class Hash(Processor):
     """(md5|sha1|sha224|sha256|sha384|sha512|crypt) <string> [<salt>]"""
     feature = 'hash'
 
-    def setup(self):
-        self.hash.im_func.pattern = re.compile(r'^(%s)\s+(.+?)$' % '|'.join(hashes.keys()), re.I)
-
-    @handler
+    @match(r'(md5|sha1|sha224|sha256|sha384|sha512)\s+(.+?)$')
     def hash(self, event, hash, string):
         hash = hash.lower()
-        event.addresponse(hashes[hash](string).hexdigest())
+        event.addresponse(eval('hashlib.%s' % hash)(string).hexdigest())
 
     @match(r'^crypt\s+(.+)\s+(\S+)$')
     def handle_crypt(self, event, string, salt):
