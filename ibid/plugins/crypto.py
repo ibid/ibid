fad9990dd0b3dffd69d4bc4daa43184892ccfe98
Michael Gorven <michael@gorven.za.net>
2009-01-07 22:46:54 +0200

    Add commands to generate random numbers, and to encode/decode base 16, 32 and 64.

--- ibid/plugins/crypto.py
+++ ibid/plugins/crypto.py
@@ -1,5 +1,6 @@
 from crypt import crypt
 import hashlib
+import base64
 import re
 
 import ibid
@@ -20,5 +21,14 @@ class Hash(Processor):
     @match(r'^crypt\s+(.+)\s+(\S+)$')
     def handle_crypt(self, event, string, salt):
         event.addresponse(crypt(string, salt))
+
+help['base64'] = 'Encodes and decodes base 16, 32 and 64.'
+class Base64(Processor):
+    """b(16|32|64)(encode|decode) <string>"""
+    feature = 'base64'
+
+    @match(r'b(16|32|64)(enc|dec)(?:ode)?\s+(.+?)$')
+    def base64(self, event, base, operation, string):
+        event.addresponse(eval('base64.b%s%sode' % (base, operation))(string))
 
 # vi: set et sta sw=4 ts=4:
--- ibid/plugins/tools.py
+++ ibid/plugins/tools.py
@@ -1,4 +1,5 @@
 import re
+from random import random, randint
 
 import ibid
 from ibid.plugins import Processor, match
@@ -13,3 +14,17 @@ class ReTest(Processor):
 	@match('^does\s+(.+?)\s+match\s+(.+?)$')
 	def retest(self, event, regex, string):
 		event.addresponse(re.search(regex, string) and 'Yes' or 'No')
+
+help['random'] = 'Generates random numbers.'
+class Random(Processor):
+	"""random [ <max> | <min> <max> ]"""
+	feature = 'random'
+
+	@match('^random(?:\s+(\d+)(?:\s+(\d+))?)?$')
+	def random(self, event, begin, end):
+		if not begin and not end:
+			event.addresponse(str(random()))
+		else:
+			begin = int(begin)
+			end = end and int(end) or 0
+			event.addresponse(str(randint(min(begin,end), max(begin,end))))
