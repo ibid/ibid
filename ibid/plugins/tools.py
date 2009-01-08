e7caf67420b66f77fe0be8db9f0ce4bf79c1f333
Michael Gorven <michael@gorven.za.net>
2009-01-08 13:38:40 +0200

    Add converter between numeric bases.

--- ibid/plugins/tools.py
+++ ibid/plugins/tools.py
@@ -8,23 +8,46 @@ help = {}
 
 help['retest'] = 'Checks whether a regular expression matches a given string.'
 class ReTest(Processor):
-	"""does <pattern> match <string>"""
-	feature = 'retest'
+    """does <pattern> match <string>"""
+    feature = 'retest'
 
-	@match('^does\s+(.+?)\s+match\s+(.+?)$')
-	def retest(self, event, regex, string):
-		event.addresponse(re.search(regex, string) and 'Yes' or 'No')
+    @match('^does\s+(.+?)\s+match\s+(.+?)$')
+    def retest(self, event, regex, string):
+        event.addresponse(re.search(regex, string) and 'Yes' or 'No')
 
 help['random'] = 'Generates random numbers.'
 class Random(Processor):
-	"""random [ <max> | <min> <max> ]"""
-	feature = 'random'
+    """random [ <max> | <min> <max> ]"""
+    feature = 'random'
 
-	@match('^rand(?:om)?(?:\s+(\d+)(?:\s+(\d+))?)?$')
-	def random(self, event, begin, end):
-		if not begin and not end:
-			event.addresponse(str(random()))
-		else:
-			begin = int(begin)
-			end = end and int(end) or 0
-			event.addresponse(str(randint(min(begin,end), max(begin,end))))
+    @match('^rand(?:om)?(?:\s+(\d+)(?:\s+(\d+))?)?$')
+    def random(self, event, begin, end):
+        if not begin and not end:
+            event.addresponse(str(random()))
+        else:
+            begin = int(begin)
+            end = end and int(end) or 0
+            event.addresponse(str(randint(min(begin,end), max(begin,end))))
+
+bases = {   'bin': (lambda x: int(x, 2), lambda x: "".join(map(lambda y:str((x>>y)&1), range(8-1, -1, -1)))),
+            'hex': (lambda x: int(x, 6), hex),
+            'oct': (lambda x: int(x, 8), oct),
+            'dec': (lambda x: int(x, 10), lambda x: x),
+            'ascii': (ord, chr),
+        }
+help['base'] = 'Convert between numeric bases as well as ASCII.'
+class Base(Processor):
+    """convert <num> from <base> to <base>"""
+    feature = 'base'
+
+    @match(r'^convert\s+(\S+)\s+(?:from\s+)?(%s)\s+(?:to\s+)?(%s)$' % ('|'.join(bases.keys()), '|'.join(bases.keys())))
+    def base(self, event, number, frm, to):
+        frm = frm.lower()
+        to = to.lower()
+
+        number = bases[frm][0](number)
+        number = bases[to][1](number)
+
+        event.addresponse(str(number))
+
+# vi: set et sta sw=4 ts=4:
