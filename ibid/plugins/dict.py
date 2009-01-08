9a4d77985ebb7b3f991a3ffcb5155e5ddf3d717d
Michael Gorven <michael@gorven.za.net>
2009-01-08 17:54:43 +0200

    Add dict client.

---
+++ ibid/plugins/dict.py
@@ -0,0 +1,53 @@
+from dictclient import Connection
+
+import ibid
+from ibid.plugins import Processor, match
+
+help = {'dict': 'Defines words and checks spellings.'}
+
+class Dict(Processor):
+    """(spell|define) <word> [using (<dictionary>|<stratergy>)]
+    (dictionaries|stratergies)"""
+    feature = 'dict'
+
+    server = 'localhost'
+    port = 2628
+
+    def setup(self):
+        self.connection = Connection(self.server, self.port)
+        self.dictionaries = self.connection.getdbdescs()
+        self.stratergies = self.connection.getstratdescs()
+
+    @match(r'^define\s+(.+?)(?:\s+using\s+(.+))?$')
+    def define(self, event, word, dictionary):
+        definitions = self.connection.define(dictionary or '*', word)
+        event.addresponse(', '.join([d.getdefstr() for d in definitions]))
+
+    @match(r'spell\s+(.+?)(?:\s+using\s+(.+))?$')
+    def handle_spell(self, event, word, stratergy):
+        suggestions = self.connection.match('*', stratergy or 'soundex', word)
+        event.addresponse(', '.join([d.getword() for d in suggestions]))
+
+    @match(r'^dictionaries$')
+    def handle_dictionaries(self, event):
+        event.addresponse(', '.join(self.dictionaries.keys()))
+
+    @match(r'^stratergies$')
+    def handle_stratergies(self, event):
+        event.addresponse(', '.join(self.stratergies.keys()))
+
+    @match(r'^dictionary\s+(.+?)$')
+    def handle_dictionary(self, event, dictionary):
+        if dictionary in self.dictionaries:
+            event.addresponse(self.dictionaries[dictionary])
+        else:
+            event.addresponse(u"I don't have that response")
+
+    @match(r'^stratergy\s+(.+?)$')
+    def handle_stratergy(self, event, stratergy):
+        if stratergy in self.stratergies:
+            event.addresponse(self.stratergies[stratergy])
+        else:
+            event.addresponse(u"I don't have that response")
+
+# vi: set et sta sw=4 ts=4:
