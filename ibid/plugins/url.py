566d46ac6bdfde454b1012cba11ecdfddf67db60
Michael Gorven <michael@gorven.za.net>
2009-01-09 20:29:15 +0200

    url: Add Lengthen and Shorten processors.

--- ibid/plugins/url.py
+++ ibid/plugins/url.py
@@ -1,43 +1,84 @@
 from datetime import datetime
+from urllib2 import urlopen, HTTPRedirectHandler, build_opener, HTTPError
+import re
 
 from sqlalchemy import Column, Integer, Unicode, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from httplib2 import Http
 
 import ibid
-from ibid.plugins import Processor, match
+from ibid.plugins import Processor, match, handler
 
 Base = declarative_base()
 
 class URL(Base):
-	__tablename__ = 'urls'
+    __tablename__ = 'urls'
 
-	id = Column(Integer, primary_key=True)
-	url = Column(Unicode)
-	channel = Column(Unicode)
-	identity = Column(Integer)
-	time = Column(DateTime)
+    id = Column(Integer, primary_key=True)
+    url = Column(Unicode)
+    channel = Column(Unicode)
+    identity = Column(Integer)
+    time = Column(DateTime)
 
-	def __init__(self, url, channel, identity):
-		self.url = url
-		self.channel = channel
-		self.identity = identity
-		self.time = datetime.now()
+    def __init__(self, url, channel, identity):
+        self.url = url
+        self.channel = channel
+        self.identity = identity
+        self.time = datetime.now()
 
 class Grab(Processor):
 
-	addressed = False
-	processed = True
+    addressed = False
+    processed = True
 
-	@match(r'((?:\S+://|(?:www|ftp)\.)\S+|\S+\.(?:com|org|net|za))')
-	def grab(self, event, url):
-		if url.find('://') == -1:
-			if url.lower().startswith('ftp'):
-				url = 'ftp://%s' % url
-			else:
-				url = 'http://%s' % url
+    @match(r'((?:\S+://|(?:www|ftp)\.)\S+|\S+\.(?:com|org|net|za)\S*)')
+    def grab(self, event, url):
+        if url.find('://') == -1:
+            if url.lower().startswith('ftp'):
+                url = 'ftp://%s' % url
+            else:
+                url = 'http://%s' % url
 
-		session = ibid.databases.ibid()
-		u = URL(url, event.channel, event.identity)
-		session.add(u)
-		session.commit()
-		session.close()
+        session = ibid.databases.ibid()
+        u = URL(url, event.channel, event.identity)
+        session.add(u)
+        session.commit()
+        session.close()
+
+class Shorten(Processor):
+
+    @match(r'^shorten\s+(\S+\.\S+)$')
+    def shorten(self, event, url):
+        f = urlopen('http://is.gd/api.php?longurl=%s' % url)
+        shortened = f.read()
+        f.close()
+
+        event.addresponse(shortened)
+
+class NullRedirect(HTTPRedirectHandler):
+
+    def redirect_request(self, req, fp, code, msg, hdrs, newurl):
+        return None
+
+class Lengthen(Processor):
+
+    services = ('http://is.gd/', 'http://tinyurl.com/', 'http://ff.im/', 'http://shorl.com/', 'http://icanhaz.com/', 'http://url.omnia.za.net/', 'http://snipurl.com/', 'http://tr.im/', 'http://snipr.com/')
+
+    def setup(self):
+        self.lengthen.im_func.pattern = re.compile(r'^((?:%s)\S+)$' % '|'.join([re.escape(service) for service in self.services]), re.I)
+    
+    @handler
+    def lengthen(self, event, url):
+        opener = build_opener(NullRedirect())
+        try:
+            f = opener.open(url)
+        except HTTPError, e:
+            if e.code in (301, 302, 303, 307):
+                event.addresponse(e.hdrs['location'])
+                return
+
+        f.close()
+        event.addresponse(u"No redirect")
+                
+
+# vi: set et sta sw=4 ts=4:
