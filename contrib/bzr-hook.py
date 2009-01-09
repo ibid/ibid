3d07eefbc6ad4f21a752ba1592db9963a2d988aa
Michael Gorven <michael@gorven.za.net>
2009-01-09 15:57:33 +0200

    bzr-hook: Update for multiple repositories.

--- contrib/bzr-hook.py
+++ contrib/bzr-hook.py
@@ -1,13 +1,16 @@
+from urlparse import urlparse
 from urllib2 import urlopen
 
 from bzrlib import branch
 
-repository = '/srv/src/ibid/'
+repositories = ['/srv/src/ibid/']
 boturl = 'http://kennels.dyndns.org:8080/'
 
 def post_change_branch_tip(params):
-	if params.branch.base.endswith('//%s' % repository):
+	repository = urlparse(params.branch.base)[2]
+	if repository in repositories:
+		name = repository.split('/')[-2]
 		for revno in xrange(params.old_revno+1, params.new_revno+1):
-			urlopen('%s?m=commit+%s' % (boturl, revno)).close()
+			urlopen('%s?m=commit+%s+%s' % (boturl, revno, name)).close()
 
 branch.Branch.hooks.install_named_hook('post_change_branch_tip', post_change_branch_tip, 'Trigger Ibid to announce the commit')
