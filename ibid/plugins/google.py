e9c5144a5c5badd32d6a40b26037d85c091c23bf
Michael Gorven <michael@gorven.za.net>
2009-01-10 00:24:16 +0200

    google: Add gdefine command, and add ZA option to search.

--- ibid/plugins/google.py
+++ ibid/plugins/google.py
@@ -11,9 +11,13 @@ class Search(Processor):
     """google [for] <term>"""
     feature = 'google'
 
-    @match(r'^google\s+(\S{2}\s+)?(?:for\s+)?(.+?)$')
+    @match(r'^google\s+(?:(za)\s+)?(?:for\s+)?(.+?)$')
     def search(self, event, country, query):
-        f = urlopen(Request('http://www.google.com/search?num=3&q=%s' % quote(query), headers={'user-agent': 'Mozilla/4.5'}))
+        url = 'http://www.google.com/search?num=3&q=%s' % quote(query)
+        if country:
+            url = url + '&meta=cr%%3Dcountry%s' % country.upper()
+
+        f = urlopen(Request(url, headers={'user-agent': 'Mozilla/4.5'}))
         soup = BeautifulSoup(f.read())
         f.close()
 
@@ -44,5 +48,24 @@ class Calc(Processor):
             event.addresponse(u'No result')
         else:
             event.addresponse(font.b.string)
+
+class Define(Processor):
+    """gdefine <term>"""
+    feature = 'google'
+
+    @match(r'^gdefine\s+(.+)$')
+    def define(self, event, term):
+        f = urlopen(Request('http://www.google.com/search?num=1&q=define:%s' % quote(term), headers={'user-agent': 'Mozilla/5.0'}))
+        soup = BeautifulSoup(f.read())
+        f.close()
+
+        definitions = []
+        for li in soup.findAll('li'):
+            definitions.append('"%s"' % li.contents[0])
+
+        if definitions:
+            event.addresponse(', '.join(definitions))
+        else:
+            event.addresponse(u"Are you making up words again?")
 
 # vi: set et sta sw=4 ts=4:
