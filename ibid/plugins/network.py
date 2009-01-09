ce6d180fec5cab68592c4b9429d5d0eb706269a9
Michael Gorven <michael@gorven.za.net>
2009-01-10 00:39:17 +0200

    network: Add ping command, fix indentation.

--- ibid/plugins/network.py
+++ ibid/plugins/network.py
@@ -1,4 +1,5 @@
 import re
+from subprocess import Popen, PIPE
 
 from dns.resolver import Resolver, NoAnswer, NXDOMAIN
 from dns.reversename import from_address
@@ -10,36 +11,57 @@ ipaddr = re.compile('\d+\.\d+\.\d+\.\d+')
 
 help['dns'] = u'Performs DNS lookups'
 class DNS(Processor):
-	"""(dns|nslookup|dig) [<record type>] [for] <host> [(from|@) <nameserver>]"""
+    """(dns|nslookup|dig) [<record type>] [for] <host> [(from|@) <nameserver>]"""
 
-	feature = 'dns'
+    feature = 'dns'
 
-	@match(r'^(?:dns|nslookup|dig)(?:\s+(a|aaaa|ptr|ns|soa|cname|mx|txt|spf|srv|sshfp|cert))?\s+(?:for\s+)?(\S+?)(?:\s+(?:from\s+|@)\s*(\S+))?$')
-	def resolve(self, event, record, host, nameserver):
-		if not record:
-			if ipaddr.search(host):
-				host = from_address(host)
-				record = 'PTR'
-			else:
-				record = 'A'
+    @match(r'^(?:dns|nslookup|dig)(?:\s+(a|aaaa|ptr|ns|soa|cname|mx|txt|spf|srv|sshfp|cert))?\s+(?:for\s+)?(\S+?)(?:\s+(?:from\s+|@)\s*(\S+))?$')
+    def resolve(self, event, record, host, nameserver):
+        if not record:
+            if ipaddr.search(host):
+                host = from_address(host)
+                record = 'PTR'
+            else:
+                record = 'A'
 
-		resolver = Resolver()
-		if nameserver:
-			if not ipaddr.search(nameserver):
-				nameserver = resolver.query(nameserver, 'A')[0].address
-			resolver.nameservers = [nameserver]
+        resolver = Resolver()
+        if nameserver:
+            if not ipaddr.search(nameserver):
+                nameserver = resolver.query(nameserver, 'A')[0].address
+            resolver.nameservers = [nameserver]
 
-		try:
-			answers = resolver.query(host, str(record))
-		except NoAnswer:
-			event.addresponse(u"I couldn't find any %s records for %s" % (record, host))
-			return
-		except NXDOMAIN:
-			event.addresponse(u"I couldn't find the domain %s" % host)
-			return
+        try:
+            answers = resolver.query(host, str(record))
+        except NoAnswer:
+            event.addresponse(u"I couldn't find any %s records for %s" % (record, host))
+            return
+        except NXDOMAIN:
+            event.addresponse(u"I couldn't find the domain %s" % host)
+            return
 
-		responses = []
-		for rdata in answers:
-			responses.append(str(rdata))
+        responses = []
+        for rdata in answers:
+            responses.append(str(rdata))
 
-		event.addresponse(', '.join(responses))
+        event.addresponse(', '.join(responses))
+
+help['ping'] = 'ICMP pings the specified host.'
+class Ping(Processor):
+    """ping <host>"""
+    feature = 'ping'
+
+    ping = 'ping'
+
+    @match(r'^ping\s+(\S+)$$')
+    def handle_ping(self, event, host):
+        
+        ping = Popen([self.ping, '-q', '-c', '3', host], stdout=PIPE, stderr=PIPE)
+        output, error = ping.communicate()
+        code = ping.wait()
+
+        if code == 0:
+            event.addresponse(' '.join(output.splitlines()[-2:]))
+        else:
+            event.addresponse(error.replace('\n', ' ').replace('ping:', '', 1).strip())
+
+# vi: set et sta sw=4 ts=4:
