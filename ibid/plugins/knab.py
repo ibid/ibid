c10e547dc2ed6388cfbf4a1c18f9bf87cc98de7c
Michael Gorven <michael@gorven.za.net>
2009-01-09 20:56:35 +0200

    knab: Decrease priority to prevent masking of native Ibid commands. Fix indentation.

--- ibid/plugins/knab.py
+++ ibid/plugins/knab.py
@@ -5,40 +5,43 @@ from ibid.plugins import Processor, handler
 
 class Knab(Processor):
 
-	knabdir = '../knab/'
-	config = 'Knab.cfg'
+    knabdir = '../knab/'
+    config = 'Knab.cfg'
+    priority = 500
 
-	def __init__(self, name):
-		Processor.__init__(self, name)
+    def __init__(self, name):
+        Processor.__init__(self, name)
 
-		perl.eval('use lib "%s"' % self.knabdir)
-		perl.require('Knab::Dumper')
-		perl.require('Knab::Conf')
-		perl.require('Knab::Modules')
-		perl.require('Knab::Processor')
-		perl.eval('$::dumper=new Knab::Dumper();')
-		perl.eval('$::config = new Knab::Conf(Basedir=>"%s", Filename=>"%s");' % (self.knabdir, self.config))
-		factoidDB = perl.eval('$::config->getValue("FactoidDB/module");')
-		perl.require(factoidDB)
-		perl.eval('$::db=new %s();' % factoidDB)
-		modules = perl.callm('new', 'Knab::Modules')
-		self.processor = perl.callm('new', 'Knab::Processor', modules)
+        perl.eval('use lib "%s"' % self.knabdir)
+        perl.require('Knab::Dumper')
+        perl.require('Knab::Conf')
+        perl.require('Knab::Modules')
+        perl.require('Knab::Processor')
+        perl.eval('$::dumper=new Knab::Dumper();')
+        perl.eval('$::config = new Knab::Conf(Basedir=>"%s", Filename=>"%s");' % (self.knabdir, self.config))
+        factoidDB = perl.eval('$::config->getValue("FactoidDB/module");')
+        perl.require(factoidDB)
+        perl.eval('$::db=new %s();' % factoidDB)
+        modules = perl.callm('new', 'Knab::Modules')
+        self.processor = perl.callm('new', 'Knab::Processor', modules)
 
-	@handler
-	def handler(self, event):
-		event.input = event.message
-		event.oldinput = event.message_raw
-		event.withpunc = event.message_raw
-		self.processor.Process(event)
+    @handler
+    def handler(self, event):
+        event.input = event.message
+        event.oldinput = event.message_raw
+        event.withpunc = event.message_raw
+        self.processor.Process(event)
 
-		responses = []
-		for response in event.responses:
-			if 'items' in dir(response):
-				new = {}
-				for key, value in response.items():
-					new[key] = value
-				responses.append(new)
-			else:
-				responses.append(response)
-				
-		event.responses = responses
+        responses = []
+        for response in event.responses:
+            if 'items' in dir(response):
+                new = {}
+                for key, value in response.items():
+                    new[key] = value
+                responses.append(new)
+            else:
+                responses.append(response)
+                
+        event.responses = responses
+
+# vi: set et sta sw=4 ts=4:
