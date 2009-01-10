dd4cd7ce8213fffc1abf15b40ed92aa376cb7be0
Michael Gorven <michael@gorven.za.net>
2009-01-10 11:15:44 +0200

    memo: Check memos for all identities, fix leaving memos for an account username.

--- ibid/plugins/memo.py
+++ ibid/plugins/memo.py
@@ -16,7 +16,12 @@ class Tell(Processor):
         session = ibid.databases.ibid()
         to = session.query(Identity).filter(func.lower(Identity.identity)==who.lower()).filter_by(source=event.source).first()
         if not to:
-            to = session.query(Account).filter(func.lower(Account.username)==who.lower()).first()
+            account = session.query(Account).filter(func.lower(Account.username)==who.lower()).first()
+            for identity in account.identities:
+                if identity.source == event.source:
+                    to = identity
+            if not identity:
+                identity = account.identities[0]
         if not to:
             event.addresponse(u"I don't know who %s is" % who)
             return
@@ -36,7 +41,12 @@ class Deliver(Processor):
     @handler
     def deliver(self, event):
         session = ibid.databases.ibid()
-        memos = session.query(Memo).filter_by(delivered=False).filter_by(to=event.identity).all()
+        if event.account:
+            account = session.query(Account).get(event.account)
+            identities = [identity.id for identity in account.identities]
+        else:
+            identities = (event.identity,)
+        memos = session.query(Memo).filter_by(delivered=False).filter(Memo.to.in_(identities)).all()
         for memo in memos:
             message = 'By the way, %s told me to tell you %s %s ago' % (memo.sender.identity, memo.memo, ago(datetime.now()-memo.time))
             if memo.private:
