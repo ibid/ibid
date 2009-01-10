daf6499b048d6f7cc3849acfe19f0e6f0f31ada4
Michael Gorven <michael@gorven.za.net>
2009-01-10 18:51:03 +0200

    google: Add compare.

--- ibid/plugins/google.py
+++ ibid/plugins/google.py
@@ -7,9 +7,13 @@ from ibid.plugins import Processor, match
 
 help = {'google': 'Retrieves results from Google and Google Calculator.'}
 
+user_agent = 'Mozilla/5.0'
+
 class Search(Processor):
     """google [for] <term>"""
     feature = 'google'
+
+    user_agent = user_agent
 
     @match(r'^google\s+(?:(za)\s+)?(?:for\s+)?(.+?)$')
     def search(self, event, country, query):
@@ -17,7 +21,7 @@ class Search(Processor):
         if country:
             url = url + '&meta=cr%%3Dcountry%s' % country.upper()
 
-        f = urlopen(Request(url, headers={'user-agent': 'Mozilla/4.5'}))
+        f = urlopen(Request(url, headers={'user-agent': self.user_agent}))
         soup = BeautifulSoup(f.read())
         f.close()
 
@@ -37,9 +41,11 @@ class Calc(Processor):
     """gcalc <expression>"""
     feature = 'google'
 
+    user_agent = user_agent
+
     @match(r'^gcalc\s+(.+)$')
     def calc(self, event, expression):
-        f = urlopen(Request('http://www.google.com/search?num=1&q=%s' % quote(expression), headers={'user-agent': 'Mozilla/5.0'}))
+        f = urlopen(Request('http://www.google.com/search?num=1&q=%s' % quote(expression), headers={'user-agent': self.user_agent}))
         soup = BeautifulSoup(f.read())
         f.close()
 
@@ -53,9 +59,11 @@ class Define(Processor):
     """gdefine <term>"""
     feature = 'google'
 
+    user_agent = user_agent
+
     @match(r'^gdefine\s+(.+)$')
     def define(self, event, term):
-        f = urlopen(Request('http://www.google.com/search?num=1&q=define:%s' % quote(term), headers={'user-agent': 'Mozilla/5.0'}))
+        f = urlopen(Request('http://www.google.com/search?num=1&q=define:%s' % quote(term), headers={'user-agent': self.user_agent}))
         soup = BeautifulSoup(f.read())
         f.close()
 
@@ -67,5 +75,30 @@ class Define(Processor):
             event.addresponse(', '.join(definitions))
         else:
             event.addresponse(u"Are you making up words again?")
+
+class Compare(Processor):
+    """google cmp [for] <term> and <term>"""
+    feature = 'google'
+
+    user_agent = user_agent
+
+    def results(self, term):
+        f = urlopen(Request('http://www.google.com/search?num=1&q=%s' % quote(term), headers={'user-agent': self.user_agent}))
+        soup = BeautifulSoup(f.read())
+        f.close()
+
+        noresults = soup.findAll('div', attrs={'class': 'med'})
+        if noresults and len(noresults) > 1 and noresults[1].find('did not match any documents') != -1:
+            return 0
+        else:
+            results = soup.find('div', id='prs').nextSibling.contents[5].string.replace(',', '')
+            if results:
+                return int(results)
+
+    @match(r'^google\s+cmp\s+(?:for\s+)?(.+?)\s+and\s+(.+?)$')
+    def compare(self, event, term1, term2):
+        count1 = self.results(term1)
+        count2 = self.results(term2)
+        event.addresponse(u'%s wins with %s hits, %s had %s hits' % (count1 > count2 and term1 or term2, count1 > count2 and count1 or count2, count1 > count2 and term2 or term1, count1 > count2 and count2 or count1))
 
 # vi: set et sta sw=4 ts=4:
