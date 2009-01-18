85839f9f8243a6fd39db07108976a5a2ebb98956
Michael Gorven <michael@gorven.za.net>
2009-01-18 14:38:50 +0200

    Flesh out ibid-setup to create a config file. Move default config files to ibid/, but symlink to / for now.

--- ibid/config.py
+++ ibid/config.py
@@ -2,7 +2,7 @@ import logging
 
 from configobj import ConfigObj, Section
 from validate import Validator
-from pkg_resources import resource_string
+from pkg_resources import resource_stream
 
 def monkeypatch(self, name):
     if self.has_key(name):
@@ -12,8 +12,8 @@ def monkeypatch(self, name):
 ConfigObj.__getattr__ = monkeypatch
 
 def FileConfig(filename):
-    spec = resource_string(__name__, 'configspec.ini')
-    configspec = ConfigObj(spec.splitlines(), list_values=False, encoding='utf-8')
+    spec = resource_stream(__name__, 'configspec.ini')
+    configspec = ConfigObj(spec, list_values=False, encoding='utf-8')
     config = ConfigObj(filename, configspec=configspec, interpolation='Template', encoding='utf-8')
     config.validate(Validator())
     logging.getLogger('core.config').info(u"Loaded configuration from %s", filename)
