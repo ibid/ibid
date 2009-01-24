250fc11191d69cc026441b9cec27fb8ec7b456b9
Michael Gorven <michael@gorven.za.net>
2009-01-24 15:56:35 +0200

    Correct indentation.

--- ibid/source/http.py
+++ ibid/source/http.py
@@ -9,40 +9,42 @@ from ibid.source import IbidSourceFactory
 from ibid.event import Event
 
 class IbidRequest(resource.Resource):
-	isLeaf = True
+    isLeaf = True
 
-	def __init__(self, name, *args, **kwargs):
-		resource.Resource.__init__(self, *args, **kwargs)
-		self.name = name
-		self.log = logging.getLogger('source.%s' % name)
+    def __init__(self, name, *args, **kwargs):
+        resource.Resource.__init__(self, *args, **kwargs)
+        self.name = name
+        self.log = logging.getLogger('source.%s' % name)
 
-	def render_GET(self, request):
-		event = Event(self.name, u'message')
-		event.who = event.sender_id = event.sender = event.channel = request.transport.getPeer().host
-		event.addressed = True
-		event.public = False
-		event.message = request.args['m'][0]
-		self.log.debug(u"Received GET request from %s: %s", event.sender, event.message)
-		ibid.dispatcher.dispatch(event).addCallback(self.respond, request)
-		return server.NOT_DONE_YET
+    def render_GET(self, request):
+        event = Event(self.name, u'message')
+        event.who = event.sender_id = event.sender = event.channel = request.transport.getPeer().host
+        event.addressed = True
+        event.public = False
+        event.message = request.args['m'][0]
+        self.log.debug(u"Received GET request from %s: %s", event.sender, event.message)
+        ibid.dispatcher.dispatch(event).addCallback(self.respond, request)
+        return server.NOT_DONE_YET
 
-	def respond(self, event, request):
-		output = '\n'.join([response['reply'].encode('utf-8') for response in event.responses])
-		request.write(output)
-		request.finish()
-		self.log.debug(u"Responded to request from %s: %s", event.sender, output)
+    def respond(self, event, request):
+        output = '\n'.join([response['reply'].encode('utf-8') for response in event.responses])
+        request.write(output)
+        request.finish()
+        self.log.debug(u"Responded to request from %s: %s", event.sender, output)
 
 class SourceFactory(IbidSourceFactory):
 
-	port = 8080
-	host = 'localhost'
+    port = 8080
+    host = 'localhost'
 
-	def __init__(self, name):
-		IbidSourceFactory.__init__(self, name)
-		self.site = server.Site(IbidRequest(self.name))
+    def __init__(self, name):
+        IbidSourceFactory.__init__(self, name)
+        self.site = server.Site(IbidRequest(self.name))
 
-	def setServiceParent(self, service):
+    def setServiceParent(self, service):
             if service:
                 return internet.TCPServer(self.port, self.site).setServiceParent(service)
             else:
                 reactor.listenTCP(self.port, self.site)
+
+# vi: set et sta sw=4 ts=4:
