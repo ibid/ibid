13a83926d00e7c727cc0201516af61b6264eb93a
Michael Gorven <michael@gorven.za.net>
2009-02-03 10:00:32 +0200

    apt: Add apt-file command.

--- ibid/plugins/apt.py
+++ ibid/plugins/apt.py
@@ -23,4 +23,23 @@ class Aptitude(Processor):
             else:
                 event.addresponse(u'No packages found')
     
+help['apt-file'] = u'Searches for packages containing the specified file'
+class AptFile(Processor):
+    """apt-file [search] <term>"""
+    feature = 'apt-file'
+
+    aptfile = Option('apt-file', 'Path to apt-file executable', 'apt-file')
+
+    @match(r'^apt-?file\s+(?:search\s+)?(.+)$')
+    def search(self, event, term):
+        apt = Popen([self.aptfile, 'search', term], stdout=PIPE, stderr=PIPE)
+        output, error = apt.communicate()
+        code = apt.wait()
+
+        if code == 0:
+            if output:
+                event.addresponse(u', '.join(line.strip() for line in output.splitlines()))
+            else:
+                event.addresponse(u'No packages found')
+
 # vi: set et sta sw=4 ts=4:
