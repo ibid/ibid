927d5176ccfe29d2a3708c006344db5574f34a4a
Michael Gorven <michael@gorven.za.net>
2009-02-03 19:32:43 +0200

    Add math plugin using bc.

---
+++ ibid/plugins/math.py
@@ -0,0 +1,25 @@
+from subprocess import Popen, PIPE
+
+from ibid.plugins import Processor, match, Option
+
+help = {}
+
+help['calc'] = 'Returns the anwser to mathematical expressions'
+class BC(Processor):
+    """<expression>"""
+
+    feature = 'calc'
+    priority = 500
+
+    bc = Option('bc', 'Path to bc executable', 'bc')
+
+    @match(r'^(.+)$')
+    def calculate(self, event, expression):
+        bc = Popen([self.bc, '-l'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, error = bc.communicate(expression.encode('utf-8') + '\n')
+        code = bc.wait()
+
+        if code == 0:
+            event.addresponse(output)
+
+# vi: set et sta sw=4 ts=4:
