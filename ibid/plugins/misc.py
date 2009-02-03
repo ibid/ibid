75d2ed8d21eb17e2d7c31c2ae66422b0f2ed112c
Michael Gorven <michael@gorven.za.net>
2009-02-03 21:05:57 +0200

    misc: Add version command.

--- ibid/plugins/misc.py
+++ ibid/plugins/misc.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+from pkg_resources import resource_exists, resource_string
+
 from ibid.plugins import Processor, match
 from ibid.config import IntOption
 
@@ -41,5 +43,16 @@ class Coffee(Processor):
             event.addresponse(True)
 
         return event
+
+version = resource_exists(__name__, '../.version') and resource_string(__name__, '../.version') or None
+
+help['version'] = u"Show the Ibid version currently running"
+class Version(Processor):
+    """version"""
+    feature = 'version'
+
+    @match(r'^version$')
+    def show_version(self, event):
+        event.addresponse(version and u"I am version %s" % version or u"I don't know what version I am :-(")
 
 # vi: set et sta sw=4 ts=4:
