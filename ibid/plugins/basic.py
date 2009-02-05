e0248b2afe17587e00577e3191ecea8977cd91a3
Michael Gorven <michael@gorven.za.net>
2009-02-05 09:54:27 +0200

    basic.SayDo: Use normal auth mechanism.

--- ibid/plugins/basic.py
+++ ibid/plugins/basic.py
@@ -12,19 +12,14 @@ class SayDo(Processor):
     """(say|do) <channel> <text>"""
     feature = 'saydo'
 
+    permission = u'saydo'
+
     @match(r'^(say|do)\s+(\S+)\s+(.*)$')
+    @authorise
     def saydo(self, event, action, where, what):
-        if ibid.auth.authenticate(event) and ibid.auth.authorise(event, u'saydo'):
-            if action.lower() == u"say":
-                reply = {'target': where, 'reply': what}
-            else:
-                reply = {'target': where, 'reply': what, 'action': True}
-        else:
-            reply = u"No!  You're not the boss of me!"
-            if action.lower() == "say":
-                event['responses'].append({'target': where, 'reply': u"Ooooh! %s was trying to make me say '%s'!" % (event.who, what)})
-            else:
-                event['responses'].append({'target': where, 'reply': u"refuses to do '%s' for '%s'" % (what, event.who), 'action': True})
+        reply = {'target': where, 'reply': what}
+        if action.lower() == u"say":
+            reply['action'] = True
 
         event.addresponse(reply)
         return event
