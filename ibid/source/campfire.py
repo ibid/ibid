6b4d7e16f6e296559ee45b8ef351ed806417840c
Michael Gorven <michael@gorven.za.net>
2009-02-19 16:40:20 +0200

    campfire: Change URL to avoid HTTPS->HTTP redirect problem. Add logging and some exception handling.

--- ibid/source/campfire.py
+++ ibid/source/campfire.py
@@ -1,3 +1,6 @@
+import logging
+from urlparse import urlparse
+
 from pinder import Campfire
 from twisted.application import internet
 
@@ -12,25 +15,30 @@ class CampfireBot(Campfire):
     def __init__(self, factory):
         self.factory = factory
         super(CampfireBot, self).__init__(self.factory.subdomain)
+        self.uri = urlparse('http://%s.campfirenow.com' % self.factory.subdomain)
         self.rooms = {}
         self.factory.join = self.join
         self.factory.part = self.part
         self.factory.send = self.send
 
     def run_one(self):
-        if not self.logged_in and not self.login(self.factory.username, self.factory.password):
-            raise AuthException(u"Login failed")
+        if not self.logged_in:
+            if not self.login(self.factory.username, self.factory.password):
+                raise AuthException(u"Login failed")
+            for room in self.factory.rooms:
+                self.join(room)
 
-        for room in self.rooms:
+        for room in self.rooms.values():
             for message in room.messages():
+                self.factory.log.debug('Received message: %s', str(message))
                 self._create_message(message, room.name)
 
-    def _create_message(message, room):
+    def _create_message(self, message, room):
         event = Event(self.factory.name, u'message')
-        event.sender = event.sender_id = message['user_id']
-        event.who = message['username']
-        event.message = message['message']
-        event.channel = room
+        event.sender = event.sender_id = unicode(message['user_id'])
+        event.who = unicode(message['username'])
+        event.message = unicode(message['message'])
+        event.channel = unicode(room)
         event.public = True
         ibid.dispatcher.dispatch(event).addCallback(self.respond)
 
@@ -42,19 +50,23 @@ class CampfireBot(Campfire):
         if response['target'] not in self.rooms:
             raise SourceException(u"Invalid room")
 
-        self.speak(response['reply'].encode('utf-8'))
+        self.rooms[response['target']].speak(response['reply'].encode('utf-8'))
+        self.factory.log.debug(u"Sent message to %s: %s", response['target'], response['reply'])
 
     def join(self, room):
-        room = find_or_create_room_by_name(room)
+        self.factory.log.info(u"Joining %s", room)
+        room = self.find_or_create_room_by_name(room)
         if not room:
             return False
 
+        room.join()
         self.rooms[room.name] = room
 
     def part(self, room):
         if room not in self.rooms:
             return False
 
+        self.factory.log.info(u"Leaving %s", room)
         self.rooms[room].leave()
         del self.rooms[room]
 
@@ -63,13 +75,18 @@ class SourceFactory(IbidSourceFactory):
     subdomain = Option('subdomain', 'Campfire subdomain')
     username = Option('username', 'Email address')
     password = Option('password', 'Campfire password')
+    rooms = Option('rooms', 'Rooms to join', [])
 
     def __init__(self, name):
         super(SourceFactory, self).__init__(name)
         self.bot = CampfireBot(self)
+        self.log = logging.getLogger('source.%s' % self.name)
 
     def run_one(self):
-        self.bot.run_one()
+        try:
+            self.bot.run_one()
+        except Exception, e:
+            self.log.exception(u"Error while polling")
 
     def setServiceParent(self, service):
         self.timer = internet.TimerService(5, self.run_one)
