517393f9033baf9f4a3fc304b7dd089e45b9820d
Michael Gorven <michael@gorven.za.net>
2009-02-21 13:48:08 +0200

    Add Twitter/identi.ca processor.

--- ibid/plugins/lookup.py
+++ ibid/plugins/lookup.py
@@ -1,11 +1,14 @@
 from urllib2 import urlopen
 from time import time
 from datetime import datetime
+from simplejson import loads
+import re
 
 import feedparser
 from BeautifulSoup import BeautifulSoup
 
-from ibid.plugins import Processor, match
+from ibid.plugins import Processor, match, handler
+from ibid.config import Option
 from ibid.utils import ago
 
 class Bash(Processor):
@@ -41,10 +44,53 @@ class FMyLife(Processor):
         f.close()
 
         quote = soup.find('div', id='wrapper').div.p
-        return quote and quote.contents[0] or None
+        return quote and u'"%s"' % (quote.contents[0],) or None
 
     @match(r'^(?:fml\s+|http://www\.fmylife\.com/\S+/)(\d+)$')
     def fml(self, event, id):
         event.addresponse(self.remote_get(int(id)) or u"No such quote")
+
+class Twitter(Processor):
+
+    default = { 'twitter': 'http://twitter.com/',
+                'tweet': 'http://twitter.com/',
+                'identica': 'http://identi.ca/api/',
+                'identi.ca': 'http://identi.ca/api/',
+              }
+    services = Option('services', 'Micro blogging services', default)
+
+    def setup(self):
+        self.update.im_func.pattern = re.compile(r'^(%s)\s+(\d+)$' % ('|'.join(self.services.keys()),))
+        self.latest.im_func.pattern = re.compile(r'^(?:latest|last)\s+(%s)\s+(?:update\s+)?(?:by\s+|from\s+)?(\S+)$' % ('|'.join(self.services.keys()),))
+
+    def remote_update(self, service, id):
+        f = urlopen('%sstatuses/show/%s.json' % (self.services[service], id))
+        status = loads(f.read())
+        f.close()
+
+        return u'%s: "%s"' % (status['user']['screen_name'], status['text'])
+
+    def remote_latest(self, service, user):
+        f = urlopen('%sstatuses/user_timeline/%s.json?count=1' % (self.services[service], user))
+        statuses = loads(f.read())
+        f.close()
+
+        return u'"%s"' % (statuses[0]['text'])
+
+    @handler
+    def update(self, event, service, id):
+        event.addresponse(self.remote_update(service.lower(), int(id)))
+
+    @handler
+    def latest(self, event, service, user):
+        event.addresponse(self.remote_latest(service.lower(), user))
+
+    @match(r'^https?://(?:www\.)?twitter\.com/[^/ ]+/statuse?s?/(\d+)$')
+    def twitter(self, event, id):
+        event.addresponse(self.remote_update('twitter', int(id)))
+
+    @match(r'^https?://(?:www\.)?identi.ca/notice/(\d+)$')
+    def identica(self, event, id):
+        event.addresponse(self.remote_update('identi.ca', int(id)))
 
 # vi: set et sta sw=4 ts=4:
