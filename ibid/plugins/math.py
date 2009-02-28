e19b3a95437e47924f9ce52ecda2f4ffec398eb3
Stefano Rivera <stefano@rivera.za.net>
2009-02-28 15:09:31 +0200

    Bases: Factor out common code and make interfaces as similar as possible. Improve error handling and messages.

--- ibid/plugins/math.py
+++ ibid/plugins/math.py
@@ -1,6 +1,7 @@
 import re
 from subprocess import Popen, PIPE
 
+import ibid
 from ibid.plugins import Processor, match, handler
 from ibid.config import Option
 
@@ -56,12 +57,13 @@ class Calc(Processor):
 
 help['base'] = 'Convert numbers between bases (radixes)'
 class BaseConvert(Processor):
-    """<number> [base <number>] (in|to) base <number>"""
-    """ascii <text> (in|to) (hex|dec|oct|bin)"""
+    """[convert] <number> [(from|in) base <number>] (in|to) base <number>
+    [convert] ascii <text> (in|to) (hex|dec|oct|bin|base <number>)
+    [convert] <sequence> (hex|dec|oct|bin) (in|to) ascii"""
 
     feature = "base"
     
-    named_bases = {
+    abbr_named_bases = {
             "hex": 16,
             "dec": 10,
             "oct": 8,
@@ -89,64 +91,93 @@ class BaseConvert(Processor):
     for value, numeral in enumerate(numerals):
         values[numeral] = value
 
-    def in_base(self, num, base):
+    def _in_base(self, num, base):
         "Recursive base-display formatter"
         if num == 0:
             return "0"
-        return self.in_base(num // base, base).lstrip("0") + self.numerals[num % base]
+        return self._in_base(num // base, base).lstrip("0") + self.numerals[num % base]
 
-    def from_base(self, num, base):
-        "Return a base-n number in decimal - int(x, n) only goes up to 36"
+    def _from_base(self, num, base):
+        "Return a base-n number in decimal. Needed as int(x, n) only works for n<=36"
+        
+        if base <= 36:
+            num = num.upper()
+
         decimal = 0
         for digit in num:
             decimal *= base
+            if self.values[digit] >= base:
+                raise ValueError("'%s' is not a valid digit in base %i" % (digit, base))
             decimal += self.values[digit]
+
         return decimal
+
+    def _parse_base(self, base):
+        "Parse a base in the form of 'hex' or 'base 13' or None"
+
+        if base is None:
+            base = 10
+        elif base[:3] in self.abbr_named_bases:
+            base = self.abbr_named_bases[base[:3]]
+        elif base.startswith(u"base"):
+            base = int(base.split()[-1])
+        else:
+            # The above should be the only cases allowed by the regex
+            # This exception indicates programmer error:
+            raise ValueError("Unparsable base: " + base)
+
+        return base
+
+    def _name_base(self, base):
+        "Shows off the bot's smartypants heritage by naming bases"
+
+        base_name = u"base %i" % base
+
+        if base in self.base_names:
+            base_name = self.base_names[base]
+
+        return base_name
 
     def setup(self):
         bases = []
-        for abbr, base in self.named_bases.iteritems():
+        for abbr, base in self.abbr_named_bases.iteritems():
             bases.append(r"%s(?:%s)?" % (abbr, self.base_names[base][3:]))
         bases = "|".join(bases)
         self.base_conversion.im_func.pattern = re.compile(
-            r"^(?:convert\s+)?([0-9a-zA-Z+/]+)\s+(?:(?:from\s+)?(base\s+\d+|%s)\s+)?(?:in|to)\s+(base\s+\d+|%s)\s*$" % (bases, bases), re.I)
+            r"^(?:convert\s+)?([0-9a-zA-Z+/]+)\s+(?:(?:(?:from|in)\s+)?(base\s+\d+|%s)\s+)?(?:in|to)\s+(base\s+\d+|%s)\s*$"
+            % (bases, bases), re.I)
+
         self.ascii_decode.im_func.pattern = re.compile(
-            r"^(?:convert\s+)?ascii\s+(.+)\s+(?:in|to)\s+(base\s+\d+|%s)$" % bases, re.I)
+            r"^(?:convert\s+)?ascii\s+(.+?)(?:(?:\s+(?:in|to))?\s+(base\s+\d+|%s))?$" % bases, re.I)
+    
+        self.ascii_encode.im_func.pattern = re.compile(
+            r"^(?:convert\s+)?([0-9a-zA-Z+/\s]+?)(?:\s+(base\s+\d+|%s))?\s+(?:in|to)\s+ascii$" % bases, re.I)
 
     @handler
     def base_conversion(self, event, number, base_from, base_to):
-        if base_from is None:
-            base_from = 10
-        elif base_from[:3] in self.named_bases:
-            base_from = self.named_bases[base_from[:3]]
-        elif base_from.startswith(u"base"):
-            base_from = int(base_from.split()[-1])
+        "Arbitrary (2 <= base <= 64) numeric base conversions."
 
-        if base_to is None:
-            base_to = 10
-        elif base_to[:3] in self.named_bases:
-            base_to = self.named_bases[base_to[:3]]
-        elif base_to.startswith(u"base"):
-            base_to = int(base_to.split()[-1])
+        base_from = self._parse_base(base_from)
+        base_to = self._parse_base(base_to)
         
-        if base_from < 2 or base_from > 64 or base_to < 2 or base_to > 64:
+        if min(base_from, base_to) < 2 or max(base_from, base_to) > 64:
             event.addresponse(u"Sorry, valid bases are between 2 and 64, inclusive.")
             return
-            
-        number = self.from_base(number, base_from)
         
-        base = u"base %i" % base_to
-        if base_to in self.base_names:
-            base = self.base_names[base_to]
-
-        event.addresponse(u"That is %s in %s." % (self.in_base(number, base_to), base))
+        try:
+            number = self._from_base(number, base_from)
+        except ValueError, e:
+            event.addresponse(e.message)
+            return
+        
+        event.addresponse(u"That is %s in %s." %
+                (self._in_base(number, base_to), self._name_base(base_to)))
 
     @handler
     def ascii_decode(self, event, text, base_to):
-        if base_to[:3] in self.named_bases:
-            base_to = self.named_bases[base_to[:3]]
-        elif base_to.startswith(u"base"):
-            base_to = int(base_to.split()[-1])
+        "Display the values of each character in an ASCII string"
+
+        base_to = self._parse_base(base_to)
 
         if len(text) > 2 and text[0] == text[-1] and text[0] in ("'", '"'):
             text = text[1:-1]
@@ -155,57 +186,53 @@ class BaseConvert(Processor):
         for char in text:
             code_point = ord(char)
             if code_point > 255:
-                output += u"U%i " % code_point
+                output += u"U%s " % self._in_base(code_point, base_to)
             else:
-                output += self.in_base(code_point, base_to) + u" "
+                output += self._in_base(code_point, base_to) + u" "
         
         output = output.strip()
 
-        event.addresponse(u"That is %s." % output)
+        event.addresponse(u"That is %s in %s." % (output, self._name_base(base_to)))
 
-    @match(r"^(?:convert\s+)?([0-9a-fA-F\s]+\s+hex(?:adecimal)?|[0-7\s]+\s+oct(?:al)?|[01\s]+\s+bin(?:ary)?|[0-9\s]+(?:\s+dec(?:imal)?)?)\s+(?:in|to)\s+ascii$")
-    def ascii_encode(self, event, source):
-        source = source.strip().split()
-        base_from = source[-1]
-        text = u""
-        if base_from in self.named_bases:
-            base_from = self.named_bases[base_from]
-            text = u" ".join(source[:-1])
-        else:
-            base_from = 10
-            text = u" ".join(source)
+        if base_to == 64 and [True for plugin in ibid.processors if plugin.feature == "base64"]:
+            event.addresponse(u'If you want a base64 encoding, use the "base64" feature.')
+
+    @handler
+    def ascii_encode(self, event, source, base_from):
+
+        base_from = self._parse_base(base_from)
 
         output = u""
         buf = u""
 
         def process_buf(buf):
-            char = int(buf, base_from)
-            if char > 255:
-                raise ValueError(u"I only deal with Basic ASCII. You don't get ascii characters bigger than 127, %i is." % char)
+            char = self._from_base(buf, base_from)
+            if char > 127:
+                raise ValueError(u"I only deal with the first page of ASCII (i.e. under 127). %i is invalid." % char)
             elif char < 32:
                 return u" %s " % "NUL SOH STX EOT ENQ ACK BEL BS HT LF VT FF SO SI DLE DC1 DC2 DC2 DC4 NAK SYN ETB CAN EM SUB ESC FS GS RS US".split()[char]
             elif char == 127:
                 return u" DEL "
-            return chr(char)
+            return unichr(char)
 
-        for char in text:
-            if char == u" ":
-                if len(buf) > 0:
-                    output += process_buf(buf)
-                    buf = u""
+        try:
+            for char in source:
+                if char == u" ":
+                    if len(buf) > 0:
+                        output += process_buf(buf)
+                        buf = u""
                 else:
-                    output += u" "
-            else:
-                buf += char
-                if (len(buf) == 2 and base_from == 16) or (len(buf) == 3 and base_from == 8) or (len(buf) == 8 and base_from == 2):
-                    output += process_buf(buf)
-                    buf = u""
+                    buf += char
+                    if (len(buf) == 2 and base_from == 16) or (len(buf) == 3 and base_from == 8) or (len(buf) == 8 and base_from == 2):
+                        output += process_buf(buf)
+                        buf = u""
 
-        if len(buf) > 0:
-            output += process_buf(buf)
+            if len(buf) > 0:
+                output += process_buf(buf)
+        except ValueError, e:
+            event.addresponse(e.message)
+            return
         
-        output = output.strip()
-
         event.addresponse(u'That is "%s".' % output)
 
 # vi: set et sta sw=4 ts=4:
