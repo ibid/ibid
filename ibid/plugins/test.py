d349d1025eecd61851552a8ed5736f2da59ff661
Stefano Rivera <stefano@rivera.za.net>
2009-03-01 12:23:47 +0200

    Some work on ibid-plugin script: * Use the same exeption path as the Dispatcher 	- Include an exception test * Allow more than one plugin to be loaded * Detect the encoding of the terminal * Name the test channel (unicode) * Exit cleanly

--- ibid/plugins/test.py
+++ ibid/plugins/test.py
@@ -27,4 +27,11 @@ class Email(Processor):
         event.addresponse({'reply': 'Test message', 'source': 'email', 'target': unicode(address)})
         event.addresponse(u"I've emailed %s" % address)
 
+class Except(Processor):
+
+    @match(r'^raise\s+exception$')
+    def handler(self, event):
+        event.addresponse("I'll except in a moment")
+        raise Exception("Ow, that hurt.")
+
 # vi: set et sta sw=4 ts=4:
