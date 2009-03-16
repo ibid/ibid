8da43e12e6321a4de628cacff2e566138115edc8
Michael Gorven <michael@gorven.za.net>
2009-03-16 22:00:15 +0200

    Only reject messages where the body is None.

--- ibid/source/jabber.py
+++ ibid/source/jabber.py
@@ -77,7 +77,7 @@ class JabberBot(xmppim.MessageProtocol, xmppim.PresenceClientProtocol, xmppim.Ro
                 self.parent.log.info(u"Ignoring message because sender is not in accept_domains")
                 return
 
-        if not message.body:
+        if message.body is None:
             self.parent.log.info(u'Ignoring empty message')
             return
 
