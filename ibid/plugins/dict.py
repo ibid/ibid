06b59159b4aa4b1078974cce43b1e76c784c6ac6
Stefano Rivera <stefano@rivera.za.net>
2009-03-20 22:29:56 +0200

    Don't keep the dictionary connection + error handling

--- ibid/plugins/dict.py
+++ ibid/plugins/dict.py
@@ -1,3 +1,5 @@
+from socket import error
+
 from dictclient import Connection
 
 from ibid.plugins import Processor, match
@@ -6,7 +8,8 @@ from ibid.config import Option, IntOption
 help = {'dict': u'Defines words and checks spellings.'}
 
 class Dict(Processor):
-    u"""(spell|define) <word> [using (<dictionary>|<strategy>)]
+    u"""spell <word> [using <dictionary>]
+    define <word> [using <strategy>]
     (dictionaries|strategies)
     (dictionary|strategy) <name>"""
     feature = 'dict'
@@ -14,48 +17,84 @@ class Dict(Processor):
     server = Option('server', 'Dictionary server hostname', 'localhost')
     port = IntOption('port', 'Dictionary server port number', 2628)
 
-    def setup(self):
-        self.connection = Connection(self.server, self.port)
-        self.dictionaries = self.connection.getdbdescs()
-        self.strategies = self.connection.getstratdescs()
-
     @match(r'^define\s+(.+?)(?:\s+using\s+(.+))?$')
     def define(self, event, word, dictionary):
-        definitions = self.connection.define(dictionary or '*', word)
-        event.addresponse(u'%s', u', '.join([d.getdefstr() for d in definitions]))
+        try:
+            connection = Connection(self.server, self.port)
+            definitions = connection.define(dictionary or '*', word)
+            if definitions:
+                event.addresponse(u'%s', u', '.join([d.getdefstr() for d in definitions]))
+            else:
+                event.addresponse(u"I don't have a definition for that. Is it even a word?")
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
+        except Exception, e:
+            event.addresponse(u'The dictionary complained: %s' % unicode(e))
 
     @match(r'^spell\s+(.+?)(?:\s+using\s+(.+))?$')
     def handle_spell(self, event, word, strategy):
-        correct = self.connection.match('*', 'exact', word)
-        if correct:
-            event.addresponse(u'That seems correct. Carry on')
-            return
-        suggestions = self.connection.match('*', strategy or 'lev', word)
-        if suggestions:
-            event.addresponse(u'Suggestions: %s', u', '.join([d.getword() for d in suggestions]))
-        else:
-            event.addresponse(u"That doesn't seem correct, but I can't find anything to suggest")
+        try:
+            connection = Connection(self.server, self.port)
+            correct = connection.match('*', 'exact', word)
+            if correct:
+                event.addresponse(u'That seems correct. Carry on')
+                return
+            suggestions = connection.match('*', strategy or 'lev', word)
+            if suggestions:
+                event.addresponse(u'Suggestions: %s', u', '.join([d.getword() for d in suggestions]))
+            else:
+                event.addresponse(u"That doesn't seem correct, but I can't find anything to suggest")
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
+        except Exception, e:
+            event.addresponse(u'The dictionary complained: %s' % unicode(e))
 
     @match(r'^dictionaries$')
     def handle_dictionaries(self, event):
-        event.addresponse(u'Dictionaries: %s', u', '.join(sorted(self.dictionaries.keys())))
+        try:
+            connection = Connection(self.server, self.port)
+            dictionaries = connection.getdbdescs()
+            event.addresponse(u'Dictionaries: %s', u', '.join(sorted(dictionaries.keys())))
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
 
     @match(r'^strater?gies$')
     def handle_strategies(self, event):
-        event.addresponse(u'Strategies: %s', u', '.join(sorted(self.strategies.keys())))
+        try:
+            connection = Connection(self.server, self.port)
+            strategies = connection.getstratdescs()
+            event.addresponse(u'Strategies: %s', u', '.join(sorted(strategies.keys())))
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
 
     @match(r'^dictionary\s+(.+?)$')
     def handle_dictionary(self, event, dictionary):
-        if dictionary in self.dictionaries:
-            event.addresponse(u'%s', self.dictionaries[dictionary])
-        else:
-            event.addresponse(u"I don't have that dictionary")
+        try:
+            connection = Connection(self.server, self.port)
+            dictionaries = connection.getdbdescs()
+            if dictionary in dictionaries:
+                event.addresponse(u'%s', dictionaries[dictionary])
+            else:
+                event.addresponse(u"I don't have that dictionary")
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
 
     @match(r'^strater?gy\s+(.+?)$')
     def handle_strategy(self, event, strategy):
-        if strategy in self.strategies:
-            event.addresponse(u'%s', self.strategies[strategy])
-        else:
-            event.addresponse(u"I don't have that strategy")
+        try:
+            connection = Connection(self.server, self.port)
+            strategies = connection.getstratdescs()
+            if strategy in strategies:
+                event.addresponse(u'%s', strategies[strategy])
+            else:
+                event.addresponse(u"I don't have that strategy")
+        except error:
+            event.addresponse(u'Unable to connect to dictionary server')
+            raise
 
 # vi: set et sta sw=4 ts=4:
