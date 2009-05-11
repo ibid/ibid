8bb6a4db2aca02513e84a43fee8cd98645ca55ea
Stefano Rivera <stefano@rivera.za.net>
2009-05-11 18:29:14 +0200

    Allow accented nicks

--- ibid/plugins/info.py
+++ ibid/plugins/info.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import os
+from unicodedata import normalize
 
 from nickometer import nickometer
 
@@ -53,18 +54,18 @@ class Nickometer(Processor):
     @match(r'^(?:nick|lame)-?o-?meter(?:(?:\s+for)?\s+(.+?))?(\s+with\s+reasons)?$')
     def handle_nickometer(self, event, nick, wreasons):
         nick = nick or event.sender['nick']
-        try:
-            score, reasons = nickometer(str(nick))
-        except UnicodeEncodeError:
-            score, reasons = 100., ((u'Not representable in ASCII', u'infinite'),)
+        if u'\ufffd' in nick:
+            score, reasons = 100., ((u'Not UTF-8 clean', u'infinite'),)
+        else:
+            score, reasons = nickometer(normalize('NFKD', nick).encode('ascii', 'ignore'))
 
-        event.addresponse(u"%(nick)s is %(score)s%% lame", {
+        event.addresponse(u'%(nick)s is %(score)s%% lame', {
             'nick': nick,
             'score': score,
         })
         if wreasons:
             if not reasons:
-                reasons = ((u"It's a proper, sensible, nick", 0),)
+                reasons = ((u'A good, traditional nick', 0),)
             event.addresponse(u'Because: %s', u', '.join(['%s (%s)' % reason for reason in reasons]))
 
 help['man'] = u'Retrieves information from manpages.'
