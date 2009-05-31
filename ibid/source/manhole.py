c85b4663cef728672a1767fe785b1714a3f760db
Stefano Rivera <stefano@rivera.za.net>
2009-05-31 10:10:44 +0200

    Configurable username and password for manhole

--- ibid/source/manhole.py
+++ ibid/source/manhole.py
@@ -3,11 +3,13 @@ from twisted.application import internet
 from twisted.manhole.telnet import ShellFactory
 
 from ibid.source import IbidSourceFactory
-from ibid.config import IntOption
+from ibid.config import Option, IntOption
 
 class SourceFactory(ShellFactory, IbidSourceFactory):
 
     port = IntOption('port', 'Port number to listen on', 9898)
+    username = Option('username', 'Login Username', 'admin')
+    password = Option('password', 'Login Password', 'admin')
 
     def __init__(self, name):
         ShellFactory.__init__(self)
