15b88740061439cfb36169c545b9559005473b0d
Stefano Rivera <stefano@rivera.za.net>
2009-06-28 00:55:51 +0200

    Add disconnect support to manhole

--- ibid/source/manhole.py
+++ ibid/source/manhole.py
@@ -18,11 +18,16 @@ class SourceFactory(ShellFactory, IbidSourceFactory):
 
     def setServiceParent(self, service=None):
         if service:
-            return internet.TCPServer(self.port, ShellFactory()).setServiceParent(service)
+            self.listener = internet.TCPServer(self.port, self).setServiceParent(service)
+            return self.listener
         else:
-            reactor.listenTCP(self.port, self)
+            self.listener = reactor.listenTCP(self.port, self)
     
     def connect(self):
         return self.setServiceParent(None)
+
+    def disconnect(self):
+        self.listener.stopListening()
+        return True
 
 # vi: set et sta sw=4 ts=4:
