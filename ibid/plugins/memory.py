0e3af57c12ec21d28823fd43ef3f010f53a4d359
Stefano Rivera <stefano@rivera.za.net>
2009-06-28 00:50:48 +0200

    New log file format. Added Object status logging

--- ibid/plugins/memory.py
+++ ibid/plugins/memory.py
@@ -1,52 +1,104 @@
 import csv
-import datetime
-import time
+from datetime import datetime, timedelta
 import gc
 import gzip
-import logging
 import os
 import os.path
 
-from ibid.plugins import Processor, handler
-from ibid.config import Option
+import simplejson
+import objgraph
+
+import ibid
+from ibid.plugins import Processor, match
+from ibid.config import Option, IntOption
+
+help = {}
+
+help['memory'] = u'Debugging module that keeps track of memory usage'
+
+def get_memusage():
+    status = file('/proc/%i/status' % os.getpid(), 'r').readlines()
+    status = [x.strip().split(':', 1) for x in status if x.startswith('Vm')]
+    return dict((x, int(y.split()[0])) for (x, y) in status)
 
 class MemoryLog(Processor):
-    filename = Option('filename', 'Memory log filename', 'logs/memory.log')
+    
+    feature = 'memory'
+
+    mem_filename = Option('mem_filename', 'Memory log filename', 'logs/memory.log')
+    mem_interval = IntOption('mem_interval', 'Interval between memory stat logging', 1)
+    obj_filename = Option('obj_filename', 'Object Statistics log filename', 'logs/objstats.log')
+    obj_interval = IntOption('obj_interval', 'Interval between logging object statistics', 1)
 
     def setup(self):
-        if os.path.isfile(self.filename + '.10.gz'):
-            os.remove(self.filename + '.10.gz')
-        for i in range(9, 0, -1):
-            if os.path.isfile('%s.%i.gz' % (self.filename, i)):
-                os.rename('%s.%i.gz' % (self.filename, i),
-                        '%s.%i.gz' % (self.filename, i+1))
-        if os.path.isfile(self.filename):
-            o = gzip.open(self.filename + '.1.gz', 'wb')
-            i = open(self.filename, 'rb')
-            o.write(i.read())
-            o.close()
-            i.close()
-            stat = os.stat(self.filename)
-            os.utime(self.filename + '.1.gz', (stat.st_atime, stat.st_mtime))
+        fns = []
+        if self.mem_interval:
+            fns.append(self.mem_filename)
+        if self.obj_interval:
+            fns.append(self.obj_filename)
+        for filename in fns:
+            if os.path.isfile(filename + '.10.gz'):
+                os.remove(filename + '.10.gz')
+            for i in range(9, 0, -1):
+                if os.path.isfile('%s.%i.gz' % (filename, i)):
+                    os.rename('%s.%i.gz' % (filename, i),
+                            '%s.%i.gz' % (filename, i+1))
+            if os.path.isfile(filename):
+                o = gzip.open(filename + '.1.gz', 'wb')
+                i = open(filename, 'rb')
+                o.write(i.read())
+                o.close()
+                i.close()
+                stat = os.stat(filename)
+                os.utime(filename + '.1.gz', (stat.st_atime, stat.st_mtime))
 
-        self._file = file('logs/memory.log', 'w+')
-        self._csv = csv.writer(self._file)
-        self._startup = time.clock()
+        if self.mem_interval:
+            self.mem_file = file(self.mem_filename, 'w+')
+            self.mem_file.write('Ibid Memory Log v2: %s\n' % ibid.config['botname'])
+            self.mem_csv = csv.writer(self.mem_file)
+            self.mem_last = datetime.utcnow()
+
+        if self.obj_interval:
+            self.obj_file = file(self.obj_filename, 'w+')
+            self.obj_file.write('Ibid Object Log v1: %s\n' % ibid.config['botname'])
+            self.obj_last = datetime.utcnow()
 
     def process(self, event):
-        status = file('/proc/%i/status' % os.getpid(), 'r').readlines()
-        status = dict(x.strip().split(':', 1) for x in status)
+        now = datetime.utcnow()
+        if self.mem_interval and now - self.mem_last >= timedelta(seconds=self.mem_interval):
+            self.mem_log()
+            self.mem_last = now
+        if self.obj_interval and now - self.obj_last >= timedelta(seconds=self.obj_interval):
+            self.obj_log()
+            self.obj_last = now
 
+    def mem_log(self):
+        status = get_memusage()
         gc.collect()
 
-        self._csv.writerow((
-            datetime.datetime.utcnow().isoformat(),
-            time.clock() - self._startup,
+        self.mem_csv.writerow((
+            datetime.utcnow().isoformat(),
             len(gc.get_objects()),
-            status['VmSize'].strip().split()[0],
-            status['VmRSS'].strip().split()[0],
-            status['VmData'].strip().split()[0],
+            status['VmSize'],
+            status['VmRSS'],
         ))
-        self._file.flush()
+        self.mem_file.flush()
+
+    def obj_log(self):
+        stats = objgraph.typestats()
+        self.obj_file.write('%s %s\n' % (
+            datetime.utcnow().isoformat(),
+            simplejson.dumps(objgraph.typestats())
+        ))
+        self.obj_file.flush()
+
+class MemoryInfo(Processor):
+    u"memory usage"
+
+    feature = 'memory'
+
+    @match('^memory\s+usage$')
+    def memory_usage(self, event):
+        event.addresponse(u"Today, I weigh in at %(VmSize)i kiB Virtual, %(VmRSS)s kiB RSS", get_memusage())
 
 # vi: set et sta sw=4 ts=4:
