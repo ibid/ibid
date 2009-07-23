d3c1fb402b3610be962a4ac54250daf94b58de34
Stefano Rivera <stefano@rivera.za.net>
2009-07-23 11:43:20 +0200

    Reverted incorrect change to FML error handler

--- ibid/plugins/lookup.py
+++ ibid/plugins/lookup.py
@@ -175,13 +175,13 @@ class FMyLife(Processor):
         try:
             body = self.remote_get(id)
         except FMLException:
-            event.addresponse(choice(self.failure_messages) % event.sender['nick'])
+            event.addresponse(choice(self.failure_messages) % event.sender)
             return
         except HTTPError:
-            event.addresponse(choice(self.failure_messages) % event.sender['nick'])
+            event.addresponse(choice(self.failure_messages) % event.sender)
             return
         except BadStatusLine:
-            event.addresponse(choice(self.failure_messages) % event.sender['nick'])
+            event.addresponse(choice(self.failure_messages) % event.sender)
             return
 
         if body:
@@ -189,7 +189,7 @@ class FMyLife(Processor):
         elif id.isdigit():
             event.addresponse(u'No such quote')
         else:
-            event.addresponse(choice(self.failure_messages) % event.sender['nick'])
+            event.addresponse(choice(self.failure_messages) % event.sender)
 
     @match(r'^fml$')
     def fml_default(self, event):
