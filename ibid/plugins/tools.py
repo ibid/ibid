10f56470261ef5c113bd432b9c0cd7c72977ef15
Michael Gorven <michael@gorven.za.net>
2010-01-02 12:29:52 +0200

    Remove unnecessary line.

--- ibid/plugins/tools.py
+++ ibid/plugins/tools.py
@@ -202,8 +202,6 @@ class TimeZone(Processor):
             offset = timezone['rawOffset']
             return tzoffset('UTC%s%s' % (offset>=0 and '+' or '', offset), offset*3600)
 
-        return None
-
     @match(r'^when\s+is\s+((?:[0-9.:/hT -]|%s)+)(?:\s+(.+))?\s+in\s+(.+)$' % '|'.join(MONTH_SHORT+MONTH_LONG+OTHER_STUFF))
     def convert(self, event, time, from_, to):
         source = time and parse(time) or datetime.now()
