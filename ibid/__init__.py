c5897e270cac9295e806abf534d8d0d3ab8a6f6f
Stefano Rivera <stefano@rivera.za.net>
2010-01-02 18:02:46 +0200

    Use base instead of .

--- ibid/__init__.py
+++ ibid/__init__.py
@@ -6,7 +6,6 @@ from ConfigParser import SafeConfigParser
 
 import sys
 sys.path.insert(0, '%s/../lib' % dirname(__file__))
-sys.path.insert(1, '.')
 
 import twisted.python.log
 
@@ -48,6 +47,7 @@ def setup(opts, service=None):
     for key, value in opts.items():
         options[key] = value
     options['base'] = dirname(options['config'])
+    sys.path.insert(0, options['base'])
 
     # Get Twisted to log to Python logging
     twisted.python.log.startLoggingWithObserver(twisted_log)
