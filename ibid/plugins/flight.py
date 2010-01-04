cc10dc9dc591cb9b2d9905bb3ccae8cc0f270982
Marco Gallotta <marco@gallotta.co.za>
2010-01-04 21:10:00 +0200

    Move airport_search outside of AirportSearch class so that it can be shared with FlightSearch (to come)

--- ibid/plugins/flight.py
+++ ibid/plugins/flight.py
@@ -1,43 +1,46 @@
 import csv
 
-from ibid.config import Option
 from ibid.plugins import Processor, match
-from ibid.utils import human_join
+from ibid.utils import cacheable_download, human_join
 
 help = { u'airport' : u'Search for airports' }
+
+airports_url = 'http://openflights.svn.sourceforge.net/viewvc/openflights/openflights/data/airports.dat'
+
+airports = {}
+
+def read_data():
+    # File is listed as ISO 8859-1 (Latin-1) encoded on
+    # http://openflights.org/data.html, but from decoding it appears to
+    # actually be UTF8
+    filename = cacheable_download(airports_url, 'flight/airports.dat')
+    reader = csv.reader(open(filename), delimiter=',', quotechar='"')
+    for row in reader:
+        airports[int(row[0])] = [unicode(r, 'utf-8') for r in row[1:]]
+
+def airport_search(query, search_loc = True):
+    if not airports:
+        read_data()
+    if search_loc:
+        query = [unicode(q) for q in query.lower().split(' ') if q]
+    else:
+        query = [unicode(query.lower())]
+    ids = []
+    for id, airport in airports.items():
+        if search_loc:
+            data = (' '.join(c.lower() for c in airport[:5])).split(' ')
+        elif len(query[0]) == 3:
+            data = [airport[3].lower()]
+        else: # assume lenght 4 (won't break if not)
+            data = [airport[4].lower()]
+        if len(filter(lambda q: q in data, query)) == len(query):
+            ids.append(id)
+    return ids
 
 class AirportSearch(Processor):
     """airport [in] <name|location|code>"""
 
     feature = 'airport'
-
-    airports_file = Option('airports_file', 'File containing airports data', 'ibid/data/airports.dat')
-
-    def setup(self):
-        # File is listed as ISO 8859-1 (Latin-1) encoded on
-        # http://openflights.org/data.html, but from decoding it appears to
-        # actually be UTF8
-        reader = csv.reader(open(self.airports_file), delimiter=',', quotechar='"')
-        self.airports = {}
-        for row in reader:
-            self.airports[int(row[0])] = [unicode(r, 'utf-8') for r in row[1:]]
-
-    def _airport_search(self, query, search_loc):
-        if search_loc:
-            query = [unicode(q) for q in query.lower().split(' ') if q]
-        else:
-            query = [unicode(query.lower())]
-        ids = []
-        for id, airport in self.airports.items():
-            if search_loc:
-                data = (' '.join(c.lower() for c in airport[:5])).split(' ')
-            elif len(query[0]) == 3:
-                data = [airport[3].lower()]
-            else: # assume lenght 4 (won't break if not)
-                data = [airport[4].lower()]
-            if len(filter(lambda q: q in data, query)) == len(query):
-                ids.append(id)
-        return ids
 
     @match(r'^airports?\s+(in\s+)?(.+)$')
     def airport_search(self, event, search_loc, query):
@@ -45,12 +48,12 @@ class AirportSearch(Processor):
         if not search_loc and not 3 <= len(query) <= 4:
             event.addresponse(u'Airport code must be 3 or 4 characters')
             return
-        ids = self._airport_search(query, search_loc)
+        ids = airport_search(query, search_loc)
         if len(ids) == 0:
             event.addresponse(u"Sorry, I don't know that airport")
         elif len(ids) == 1:
             id = ids[0]
-            airport = self.airports[id]
+            airport = airports[id]
             code = 'unknown code'
             if airport[3] and airport[4]:
                 code = 'codes %s and %s' % (airport[3], airport[4])
@@ -63,7 +66,7 @@ class AirportSearch(Processor):
         else:
             results = []
             for id in ids:
-                airport = self.airports[id]
+                airport = airports[id]
                 code = ''
                 if airport[3] or airport[4]:
                     code = ' (%s)' % u'/'.join(filter(lambda c: c, airport[3:5]))
