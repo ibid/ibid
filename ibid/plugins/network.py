2107da51f18ad85590f188b36fa58303c783dfb2
Jonathan Hitchcock <jonathan@vhata.net>
2010-01-16 18:32:20 +0200

    Move http into network

--- ibid/plugins/network.py
+++ ibid/plugins/network.py
@@ -3,13 +3,17 @@ from subprocess import Popen, PIPE
 
 from dns.resolver import Resolver, NoAnswer, NXDOMAIN
 from dns.reversename import from_address
+from httplib import HTTPConnection, HTTPSConnection
+from urllib import getproxies_environment
+from urlparse import urlparse
 
 from ibid.plugins import Processor, match
-from ibid.config import Option
+from ibid.config import Option, IntOption
 from ibid.utils import file_in_path, unicode_output, human_join
 
 help = {}
 ipaddr = re.compile('\d+\.\d+\.\d+\.\d+')
+title = re.compile(r'<title>(.*)<\/title>', re.I+re.S)
 
 help['dns'] = u'Performs DNS lookups'
 class DNS(Processor):
@@ -165,5 +169,51 @@ class IPCalc(Processor):
         else:
             error = unicode_output(error.strip())
             event.addresponse(error.replace(u'\n', u' '))
+
+help['get'] = u'Retrieves a URL and returns the HTTP status and optionally the HTML title.'
+class HTTP(Processor):
+    u"""(get|head) <url>"""
+    feature = 'get'
+
+    max_size = IntOption('max_size', 'Only request this many bytes', 500)
+
+    @match(r'^(get|head)\s+(\S+\.\S+)$')
+    def handler(self, event, action, url):
+        if not url.lower().startswith("http://") and not url.lower().startswith("https://"):
+            url = "http://" + url
+        if url.count("/") < 3:
+            url += "/"
+
+        action = action.upper()
+
+        scheme, host = urlparse(url)[:2]
+        scheme = scheme.lower()
+        proxies = getproxies_environment()
+        if scheme in proxies:
+            scheme, host = urlparse(proxies[scheme])[:2]
+            scheme = scheme.lower()
+
+        if scheme == "https":
+            conn = HTTPSConnection(host)
+        else:
+            conn = HTTPConnection(host)
+
+        headers={}
+        if action == 'GET':
+            headers['Range'] = 'bytes=0-%s' % self.max_size
+        conn.request(action.upper(), url, headers=headers)
+
+        response = conn.getresponse()
+        reply = u'%s %s' % (response.status, response.reason)
+
+        data = response.read()
+        conn.close()
+
+        if action == 'GET':
+            match = title.search(data)
+            if match:
+                reply += u' "%s"' % match.groups()[0].strip()
+
+        event.addresponse(reply)
 
 # vi: set et sta sw=4 ts=4:
