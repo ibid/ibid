364402a33e86257f77c4505344fa234ae5ef4649
Jonathan Hitchcock <jonathan@vhata.net>
2010-01-18 20:23:56 +0200

    config -> admin

--- ibid/plugins/admin.py
+++ ibid/plugins/admin.py
@@ -1,9 +1,14 @@
+from os.path import join
 from twisted.internet import reactor
+import logging
 
 import ibid
 from ibid.utils import human_join
-from ibid.plugins import Processor, match, authorise
+from ibid.config import FileConfig
+from ibid.plugins import Processor, match, authorise, auth_responses
 from ibid.utils import ibid_version
+
+log = logging.getLogger('plugins.admin')
 
 help = {}
 
@@ -134,5 +139,72 @@ class Version(Processor):
             event.addresponse(u'I am version %s', ibid_version())
         else:
             event.addresponse(u"I don't know what version I am :-(")
+
+help['config'] = u'Gets and sets configuration settings, and rereads the configuration file.'
+class Config(Processor):
+    u"""reread config
+    set config <name> to <value>
+    get config <name>"""
+    feature = 'config'
+
+    priority = -10
+    permission = u'config'
+
+    @match(r'^reread\s+config$')
+    @authorise()
+    def reload(self, event):
+        ibid.config.reload()
+        ibid.config.merge(FileConfig(join(ibid.options['base'], 'local.ini')))
+        ibid.reloader.reload_config()
+        ibid.auth.drop_caches()
+        event.addresponse(True)
+        log.info(u'Reread configuration file')
+
+    @match(r'^(?:set\s+config|config\s+set)\s+(\S+?)(?:\s+to\s+|\s*=\s*)(\S.*?)$')
+    @authorise()
+    def set(self, event, key, value):
+        config = ibid.config
+        for part in key.split('.')[:-1]:
+            if isinstance(config, dict):
+                if part not in config:
+                    config[part] = {}
+            else:
+                event.addresponse(u'No such option')
+                return
+
+            config = config[part]
+
+        part = key.split('.')[-1]
+        if not isinstance(config, dict):
+            event.addresponse(u'No such option')
+            return
+        if ',' in value:
+            config[part] = [part.strip() for part in value.split(',')]
+        else:
+            config[part] = value
+
+        ibid.config.write()
+        ibid.reloader.reload_config()
+        log.info(u"Set %s to %s", key, value)
+
+        event.addresponse(True)
+
+    @match(r'^(?:get\s+config|config\s+get)\s+(\S+?)$')
+    def get(self, event, key):
+        if 'password' in key.lower() and not auth_responses(event, u'config'):
+            return
+
+        config = ibid.config
+        for part in key.split('.'):
+            if not isinstance(config, dict) or part not in config:
+                event.addresponse(u'No such option')
+                return
+            config = config[part]
+        if isinstance(config, list):
+            event.addresponse(u', '.join(config))
+        elif isinstance(config, dict):
+            event.addresponse(u'Keys: ' + human_join(config.keys()))
+        else:
+            event.addresponse(unicode(config))
 
 # vi: set et sta sw=4 ts=4:
