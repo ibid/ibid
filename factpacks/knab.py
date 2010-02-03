07187d324b334ebb12b26c1ad2575f31c38216cb
Stefano Rivera <stefano@rivera.za.net>
2010-02-03 18:06:30 +0200

    Copyright statements for factpack-generation scripts

--- factpacks/knab.py
+++ factpacks/knab.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) 2009 Michael Gorven
+# Released under terms of the MIT/X/Expat Licence. See COPYING for details.
+#
 # Usage: knab.py <filename> <factoid name> [<factoid name>...]
 
 from sys import argv
