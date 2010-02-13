a45c36bcaa45dfef03c55e3d99e0049c78f03d73
Michael Gorven <michael@gorven.za.net>
2010-02-13 10:20:20 +0200

    Make MAC address capturing stricter and allow it without keyword in front.

--- ibid/plugins/sysadmin.py
+++ ibid/plugins/sysadmin.py
@@ -187,7 +187,7 @@ class Mac(Processor):
     u"""mac <address>"""
     feature = 'mac'
 
-    @match(r'^(?:mac|oui|ether|ether(?:net)?(?:\s*code)?)\s+([0-9a-f-:]{6,})$')
+    @match(r'^(?:(?:mac|oui|ether|ether(?:net)?(?:\s*code)?)\s+)?((?:(?:[0-9a-f]{2}[:-]?){3}){1,2})$')
     def lookup_mac(self, event, mac):
         oui = mac.replace('-', '').replace(':', '').upper()[:6]
         try:
