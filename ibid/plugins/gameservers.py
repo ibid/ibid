8bde16429f58ab21716ae6f18134d2f850aad1c8
Stefano Rivera <stefano@rivera.za.net>
2010-03-19 14:24:38 +0200

    Ben wrote the initial gameservers code, which Stefano refactored

--- ibid/plugins/gameservers.py
+++ ibid/plugins/gameservers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2010, Stefano Rivera
+# Copyright (c) 2009-2010, Ben Steenhuisen, Stefano Rivera
 # Released under terms of the MIT/X/Expat Licence. See COPYING for details.
 
 import re
