84e537b03d85e17046b689906092f6cd59493ffe
Max Rabkin <max.rabkin@gmail.com>
2010-04-11 23:43:09 +0200

    Made indefinite_article more accurate

--- ibid/utils/__init__.py
+++ ibid/utils/__init__.py
@@ -279,11 +279,52 @@ def get_process_output(command, input=None):
     return output, error, code
 
 def indefinite_article(noun_phrase):
-    if noun_phrase[0] in u'aeiou' or (noun_phrase[0] == u'y'
-                                     and noun_phrase[1] not in u'aeiou'):
-        return u'an'
+    # adapted from CPAN package Lingua-EN-Inflect-1.891 by Damian Conway
+    m = re.search('\w+', noun_phrase)
+    if m:
+        word = m.group(0)
     else:
-        return u'a'
+        return 'an'
+
+    wordi = word.lower()
+    for anword in ("euler", "heir", "honest", "hono"):
+        if wordi.startswith(anword):
+            return 'an'
+
+    if wordi.startswith('hour') and not wordi.startswith('houri'):
+        return 'an'
+
+    if word in 'aedhilmnorsx':
+        return 'an'
+    elif len(word) == 1:
+        return 'a'
+
+    if re.match(r'(?!FJO|[HLMNS]Y.|RY[EO]|SQU|'
+                  r'(F[LR]?|[HL]|MN?|N|RH?|S[CHKLMNPTVW]?|X(YL)?)[AEIOU])'
+                  r'[FHLMNRSX][A-Z]', word):
+        return 'an'
+
+    for regex in (r'^e[uw]', r'^onc?e\b',
+                    r'^uni([^nmd]|mo)','^u[bcfhjkqrst][aeiou]'):
+        if re.match(regex, wordi):
+            return 'a'
+
+    # original regex was /^U[NK][AIEO]?/ but that matches UK, UN, etc.
+    if re.match('^U[NK][AIEO]', word):
+        return 'a'
+    elif word == word.upper():
+        if wordi[0] in 'aedhilmnorsx':
+            return 'an'
+        else:
+            return 'a'
+
+    if wordi[0] in 'aeiou':
+        return 'an'
+
+    if re.match(r'^y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt)', wordi):
+        return 'an'
+    else:
+        return 'a'
 
 def get_country_codes():
     filename = cacheable_download(
