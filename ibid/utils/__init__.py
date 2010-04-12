46ddc07f9ea057ea5358253f910bd70237798f6e
Max Rabkin <max.rabkin@gmail.com>
2010-04-12 23:43:43 +0200

    Return unicode from indefinite_article

--- ibid/utils/__init__.py
+++ ibid/utils/__init__.py
@@ -288,48 +288,48 @@ def indefinite_article(noun_phrase):
     if m:
         word = m.group(0)
     else:
-        return 'an'
+        return u'an'
 
     wordi = word.lower()
     for anword in ("euler", "heir", "honest", "hono"):
         if wordi.startswith(anword):
-            return 'an'
+            return u'an'
 
     if wordi.startswith('hour') and not wordi.startswith('houri'):
-        return 'an'
+        return u'an'
 
     if len(word) == 1:
         if wordi in 'aedhilmnorsx':
-            return 'an'
+            return u'an'
         else:
-            return 'a'
+            return u'a'
 
     if re.match(r'(?!FJO|[HLMNS]Y.|RY[EO]|SQU|'
                   r'(F[LR]?|[HL]|MN?|N|RH?|S[CHKLMNPTVW]?|X(YL)?)[AEIOU])'
                   r'[FHLMNRSX][A-Z]', word):
-        return 'an'
+        return u'an'
 
     for regex in (r'^e[uw]', r'^onc?e\b',
                     r'^uni([^nmd]|mo)','^u[bcfhjkqrst][aeiou]'):
         if re.match(regex, wordi):
-            return 'a'
+            return u'a'
 
     # original regex was /^U[NK][AIEO]?/ but that matches UK, UN, etc.
     if re.match('^U[NK][AIEO]', word):
-        return 'a'
+        return u'a'
     elif word == word.upper():
         if wordi[0] in 'aedhilmnorsx':
-            return 'an'
+            return u'an'
         else:
-            return 'a'
+            return u'a'
 
     if wordi[0] in 'aeiou':
-        return 'an'
+        return u'an'
 
     if re.match(r'^y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt)', wordi):
-        return 'an'
+        return u'an'
     else:
-        return 'a'
+        return u'a'
 
 def get_country_codes():
     filename = cacheable_download(
