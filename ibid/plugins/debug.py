57d5140c1959f4ad8799486cc87bb09442bb5203
Max Rabkin <max.rabkin@gmail.com>
2010-06-17 12:04:37 +0200

    fix imports and needless lock

--- ibid/plugins/debug.py
+++ ibid/plugins/debug.py
@@ -2,17 +2,15 @@
 # Released under terms of the MIT/X/Expat Licence. See COPYING for details.
 
 from random import choice
-from threading import Lock
 from traceback import format_exception, format_exception_only
 
-from ibid.plugins import Processor, match, authorise, handler
+from ibid.plugins import Processor, match, authorise
 
 features = {'debug': {
     'description': u'Help track bugs in the bot',
     'categories': ('debug',),
 }}
 
-exc_lock = Lock()
 last_exc_info = None
 
 class SetLastException(Processor):
@@ -24,11 +22,7 @@ class SetLastException(Processor):
         global last_exc_info
 
         if 'exc_info' in event:
-            exc_lock.acquire()
-            try:
-                last_exc_info = event.exc_info
-            finally:
-                exc_lock.release()
+            last_exc_info = event.exc_info
 
 class LastException(Processor):
     features = (u'debug',)
@@ -40,16 +34,12 @@ class LastException(Processor):
     @match(r'^last\s+(exception|trac[ek]back)$')
     @authorise()
     def exception(self, event, kind):
-        exc_lock.acquire()
-        try:
-            if last_exc_info is None:
-                event.addresponse(choice((u'Are you *looking* for trouble?',
-                                          u"I'll make an exception for you.")))
+        if last_exc_info is None:
+            event.addresponse(choice((u'Are you *looking* for trouble?',
+                                      u"I'll make an exception for you.")))
+        else:
+            if kind.lower() == 'exception':
+                lines = format_exception_only(*last_exc_info[:2])
             else:
-                if kind.lower() == 'exception':
-                    lines = format_exception_only(*last_exc_info[:2])
-                else:
-                    lines = format_exception(*last_exc_info)
-                event.addresponse(unicode(''.join(lines)[:-1]), conflate=False)
-        finally:
-            exc_lock.release()
+                lines = format_exception(*last_exc_info)
+            event.addresponse(unicode(''.join(lines)[:-1]), conflate=False)
