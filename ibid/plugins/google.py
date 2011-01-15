df6efa3378df5b3a484f0a9e255a5523c9b498ba
Stefano Rivera <stefano@rivera.za.net>
2011-01-15 14:14:29 +0200

    Strip all tags (naively), not just font

--- ibid/plugins/google.py
+++ ibid/plugins/google.py
@@ -117,8 +117,7 @@ class GoogleScrapeSearch(Processor):
             node = re.sub(r'^<b>(.*)</b>$', lambda x: x.group(1), node)
             node = re.sub(r'<sup>(.*?)</sup>',
                           lambda x: u'^' + x.group(1), node)
-            node = re.sub(r'<font[^>]*?>(.*?)</font>',
-                          lambda x: x.group(1), node)
+            node = re.sub(r'<[^>]*?>', '', node)
             node = decode_htmlentities(node)
             event.addresponse(node)
         else:
