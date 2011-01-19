f4197c0f92649fbc4f58ef229db47c9fe47aad12
Max Rabkin <max.rabkin@gmail.com>
2011-01-19 18:04:19 +0200

    add simple test for unihan

---
+++ ibid/test/plugins/test_conversions.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2010, Max Rabkin
+# Released under terms of the MIT/X/Expat Licence. See COPYING for details.
+
+import re
+
+from ibid.test import PluginTestCase
+
+class UnihanTest(PluginTestCase):
+    load = ['conversions']
+    network = True
+
+    def test_simp_trad(self):
+        self.assertResponseMatches(u'unihan \u9A6C',
+                                   u'.*the traditional form is \u99AC')
+        self.assertResponseMatches(u'unihan \u99AC',
+                                   u'.*the simplified form is \u9A6C')
