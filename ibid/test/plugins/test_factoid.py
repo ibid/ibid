760f6b0542580bf88fc37ba10f45ab350fb59ab0
Max Rabkin <max.rabkin@gmail.com>
2011-01-31 11:35:11 +0200

    More factoid cases

--- ibid/test/plugins/test_factoid.py
+++ ibid/test/plugins/test_factoid.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2011, Max Rabkin
 # Released under terms of the MIT/X/Expat Licence. See COPYING for details.
 
@@ -37,6 +38,11 @@ class FactoidTest(PluginTestCase):
                 self.assertResponseMatches(name,
                     '%s is %s' % (name, value.replace(needle, subst, 1)))
 
+    def test_string_transliterate(self):
+        self.assertSucceeds('foo is bar quux')
+        self.assertSucceeds('foo ~= y/a x/ ay/')
+        self.assertResponseMatches('foo', 'foo isab raquuy')
+
     def test_append(self):
         name = u'foo'
         value = 'x bar baz quux'
@@ -48,3 +54,38 @@ class FactoidTest(PluginTestCase):
             self.assertSucceeds('%s +=%s' % (name, add))
             self.assertResponseMatches(name,
                 '%s is %s' % (name, value+add))
+
+    def test_search_case(self):
+        # related to Bug #336247
+        self.assertSucceeds('foo is bar QUUX')
+        resp = r'foo \[1\]'
+        self.assertResponseMatches('search for Foo', resp)
+        self.assertResponseMatches('search for FoO', resp)
+        self.assertResponseMatches('search for values Bar', resp)
+        self.assertResponseMatches('search for Quux', resp)
+
+    def test_modify_nothing(self):
+        # test whether modifying a non-existent factoid vomits
+        self.assertSucceeds('foo ~= s/lol/rofl/')
+
+    def test_forget_wildcard(self):
+        names = ['a $arg', '$args', '$arg', 'a $arg $arg']
+        for name in names:
+            self.assertSucceeds('%s is $1' % name)
+            self.failIfResponseMatches('forget %s' % name, ".*didn't know")
+            self.assertFails(name.replace('$arg', 'foo'))
+
+    def test_unicode_name(self):
+        names = [u'ascii', u'ü' , u'Ü', u'よし']
+        for name in names:
+            self.assertSucceeds('%s is foo' % name)
+            self.assertResponseMatches(name, '%s is foo' % name)
+            self.assertSucceeds('forget %s' % name)
+            self.assertSucceeds('%s $arg is foo $1' % name)
+            self.assertResponseMatches('%s baz' % name, '%s baz is foo baz' % name)
+            self.assertSucceeds('forget %s $arg' % name)
+
+    def test_search_unescape(self):
+        self.assertSucceeds('slap $arg is <action>slaps $1')
+        self.assertResponseMatches('search for slap',
+            re.escape('slap $arg [1]'))
